8fcee792c418cd011054bf8f4ff3803530e9cbf5
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2008-09-02 16:53:52 +0000

    New link scripts to use cxxplugin as build target

--- branches/Free/OpenFlipper/Plugins/64/link.sh
+++ branches/Free/OpenFlipper/Plugins/64/link.sh
@@ -5,8 +5,10 @@ rm -f max/*
 rm -f dbg/*
 
 for i in $(ls -d $BASEPATH/Plugin*); do
-  ln -s "../$(find $i/Deb* -name *Plugin* | grep lib | grep max | grep 64 )" max
-  ln -s "../$(find $i/Deb* -name *Plugin* | grep lib | grep dbg | grep 64 )" dbg
+  ln -s "../$(find $i/Deb* -name *Plugin* | grep lib | grep max | grep 64 )" max 2>&1 | grep -v "File exists"
+  ln -s "../$(find $i/Deb* -name Plugin-*  | grep max | grep 64 )" max           2>&1 | grep -v "File exists"
+  ln -s "../$(find $i/Deb* -name *Plugin* | grep lib | grep dbg | grep 64 )" dbg 2>&1 | grep -v "File exists"
+  ln -s "../$(find $i/Deb* -name Plugin-*  | grep dbg | grep 64 )" dbg           2>&1 | grep -v "File exists"
 done
 
 
