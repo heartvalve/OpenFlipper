473e6b78a68464ee2f9c03b43abf69acd8cdbbf1
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2008-09-19 12:45:25 +0000

    Fixed bug deep in ini file writer for vectors of qstrings

--- branches/Free/OpenFlipper/INIFile/INIFile.cc
+++ branches/Free/OpenFlipper/INIFile/INIFile.cc
@@ -12,12 +12,12 @@
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  OpenFlipper is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
 //
@@ -367,8 +367,9 @@ void INIFile::add_entry( const QString & _section,
 {
   QString list;
   std::vector<QString>::const_iterator viter;
-  for(viter = _value.begin();viter!=_value.end();++viter)
-    list = *viter + ";";
+  for(viter = _value.begin();viter!=_value.end();++viter) {
+    list += *viter + ";";
+  }
   m_iniData[ _section ][ _key ] = list;
 }
 
