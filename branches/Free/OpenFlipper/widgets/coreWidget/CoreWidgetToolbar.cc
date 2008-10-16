1b27ad90d6c1e6caa05ef4e85d7adbe8b1e2898d
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2008-10-16 16:33:58 +0000

    Tried to make valgrind happy

--- branches/Free/OpenFlipper/widgets/coreWidget/CoreWidgetToolbar.cc
+++ branches/Free/OpenFlipper/widgets/coreWidget/CoreWidgetToolbar.cc
@@ -12,12 +12,12 @@
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  OpenFlipper is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
 //
@@ -48,11 +48,14 @@
 
 // -------------------- Qt
 
-//== IMPLEMENTATION ========================================================== 
+//== IMPLEMENTATION ==========================================================
 
 void CoreWidget::slotAddToolbar(QToolBar* _toolbar) {
   addToolBar( _toolbar );
-  _toolbar->setObjectName("Toolbar" + QString::number(toolbarCount_++));
+  int newNumber = toolbarCount_;
+  toolbarCount_++;
+  QString number = QString::number(newNumber);
+  _toolbar->setObjectName("Toolbar" + number);
 }
 
 void CoreWidget::slotRemoveToolbar(QToolBar* _toolbar) {
