2ee5dd58a76dccef1ae07c1d4cb12fec50575c7d
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2009-01-14 14:51:30 +0000

    Updated Documentation

--- branches/Free/OpenFlipper/BasePlugin/MouseInterface.hh
+++ branches/Free/OpenFlipper/BasePlugin/MouseInterface.hh
@@ -12,12 +12,12 @@
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  OpenFlipper is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
 //
@@ -35,68 +35,68 @@
 //
 // C++ Interface: Basic Interface for mouse events
 //
-// Description: 
+// Description:
 //
 //
 // Author: Jan Moebius <moebius@informatik.rwth-aachen.de>, (C) 2007
 
 
 
-#ifndef MOUSEINTERFACE_HH 
-#define MOUSEINTERFACE_HH 
+#ifndef MOUSEINTERFACE_HH
+#define MOUSEINTERFACE_HH
 
  #include <QtGui>
- 
-/** \brief Interface for all plugins which have to react on mouse events 
-  * 
-  * When using mouse events in your plugins implement this interface. Remember that 
+
+/** \brief Interface for all plugins which have to react on mouse events
+  *
+  * When using mouse events in your plugins implement this interface. Remember that
   * all plugins may get these signals. Therefore you hav to check if the current pickingMode
   * is active before doing anything. Only the slotMouseEventIdentify is independent of picking modes.
- */  
+ */
 class MouseInterface {
-  
+
   private slots :
-    
+
     /**  \brief Wheel Event from Main App
-      * 
+      *
       *  This slot is called if a wheel event occured in the Viewer and is extended by
       *  the current Mode set in the Viewer. You can add additional Picking Modes to
-      *  the Viewer by calling examiner_widget_->addPickMode("Mode"). 
+      *  the Viewer by using the picking interface.
       *  This slot will only get called in pickingMode.
       *  @param _event Mousevent
       *  @param _mode Name of the current Picking Mode.
     */
     virtual void slotMouseWheelEvent(QWheelEvent * /*_event*/, const std::string & /*_mode*/) {};
-    
+
     /**  \brief Mouse Event from Main App ( Picking Mode )
-      * 
-      *  This slot is called if a mouse event occured in the Viewer 
-      *  This slot will only get called in pickingMode. 
+      *
+      *  This slot is called if a mouse event occured in the Viewer
+      *  This slot will only get called in pickingMode.
       *  Right button clicks will not be passed to the plugins as this is reserved for
-      *  the context Menu. 
-      * 
+      *  the context Menu.
+      *
       *  @param _event Mousevent
     */
     virtual void slotMouseEvent( QMouseEvent* /*_event*/ ) {};
-    
+
     /**  \brief Mouse Event from Main App ( Identify Mode )
-      * 
-      *  This slot is called if a mouse event occured in the Viewer 
-      *  This slot will only get called in identifyMode. 
+      *
+      *  This slot is called if a mouse event occured in the Viewer
+      *  This slot will only get called in identifyMode.
       *  Right button clicks will not be passed to the plugins as this is reserved for
-      *  the context Menu. 
-      * 
+      *  the context Menu.
+      *
       *  @param _event Mousevent
-    */    
+    */
     virtual void slotMouseEventIdentify() {};
-    
-  public : 
-      
+
+  public :
+
     /// Destructor
     virtual ~MouseInterface() {};
-      
+
 };
 
 Q_DECLARE_INTERFACE(MouseInterface,"OpenFlipper.MouseInterface/1.0")
-      
+
 #endif // MOUSEINTERFACE_HH
