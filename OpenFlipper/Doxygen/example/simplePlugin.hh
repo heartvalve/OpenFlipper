d3693ae1275c41b67fb37011a6fd4d2d5f5c69fa
kremer <mkremer@informatik.rwth-aachen.de>
2009-01-29 13:24:36 +0000

    Updated the documentation by adding a simple example plugin and a step-by-step tutorial.

---
+++ OpenFlipper/Doxygen/example/simplePlugin.hh
@@ -0,0 +1,23 @@
+#ifndef SIMPLEPLUGIN_HH_INCLUDED
+#define SIMPLEPLUGIN_HH_INCLUDED
+
+#include <OpenFlipper/BasePlugin/BaseInterface.hh>
+#include <OpenFlipper/common/Types.hh>
+
+class SimplePlugin : public QObject, BaseInterface
+{
+Q_OBJECT
+Q_INTERFACES(BaseInterface)
+
+public :
+  
+  ~SimplePlugin() {};
+  
+  QString name() { return QString("My first plugin"); };
+  
+  QString description() { return QString("Does actually nothing but works!"); };
+  
+};
+
+
+#endif
