c374c37e35c7f0b62d05b8991f6fb1d652105e89
moebius <moebius@informatik.rwth-aachen.de>
2009-02-27 16:34:32 +0000

    License and properties

--- Plugin-Decimater/DecimaterPlugin.hh
+++ Plugin-Decimater/DecimaterPlugin.hh
@@ -1,12 +1,31 @@
 //=============================================================================
 //
-// CLASS DecimaterPlugin
+//                               OpenFlipper
+//        Copyright (C) 2008 by Computer Graphics Group, RWTH Aachen
+//                           www.openflipper.org
 //
-// Author:  David Bommes <bommes@cs.rwth-aachen.de>
+//-----------------------------------------------------------------------------
 //
-// Version: $Revision: 1$
-// Date:    $Author$
-//   $Date: 03-10-2007$
+//                                License
+//
+//  OpenFlipper is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  OpenFlipper is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
+//
+//-----------------------------------------------------------------------------
+//
+//   $Revision$
+//   $Author$
+//   $Date$
 //
 //=============================================================================
 
