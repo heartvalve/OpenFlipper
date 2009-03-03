12250f7653659bf12aa6bea1371b5623a8df5b13
wilden <wilden@383ad7c9-94d9-4d36-a494-682f7c89f535>
2009-03-03 16:33:33 +0000

    enhanced ColorPlugin and added docu
    color presets can now be chosen in the options and are stored in ini files

--- Plugin-Color/ColorPlugin.hh
+++ Plugin-Color/ColorPlugin.hh
@@ -12,12 +12,12 @@
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  OpenFlipper is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
 //
@@ -36,14 +36,20 @@
 #include <QMenuBar>
 #include "ColorPlugin.hh"
 
+#include "ColorOptions.hh"
+
+#include "OpenFlipper/INIFile/INIFile.hh"
+
 #include <OpenFlipper/common/Types.hh>
 #include <OpenFlipper/BasePlugin/BaseInterface.hh>
 #include <OpenFlipper/BasePlugin/MenuInterface.hh>
 #include <OpenFlipper/BasePlugin/ScriptInterface.hh>
 #include <OpenFlipper/BasePlugin/ToolbarInterface.hh>
 #include "OpenFlipper/BasePlugin/StatusbarInterface.hh"
+#include "OpenFlipper/BasePlugin/OptionsInterface.hh"
+#include "OpenFlipper/BasePlugin/INIInterface.hh"
 
-class ColorPlugin : public QObject, BaseInterface, MenuInterface, ScriptInterface, ToolbarInterface, StatusbarInterface
+class ColorPlugin : public QObject, BaseInterface, MenuInterface, ScriptInterface, ToolbarInterface, StatusbarInterface, OptionsInterface, INIInterface
 {
 Q_OBJECT
 Q_INTERFACES(BaseInterface)
@@ -51,29 +57,90 @@ Q_INTERFACES(MenuInterface)
 Q_INTERFACES(ScriptInterface)
 Q_INTERFACES(ToolbarInterface)
 Q_INTERFACES(StatusbarInterface)
+Q_INTERFACES(OptionsInterface)
+Q_INTERFACES(INIInterface)
 
   signals:
+    // BaseInterface
     void updateView();
 
+    // ScriptInterface
     void scriptInfo( QString _functionName );
 
+    // MenuInterface
     void addMenu(QMenu* _menu, MenuType _type);
 
+    // ToolbarInterface
     void addToolbar(QToolBar* _toolbar);
     void removeToolbar(QToolBar* _toolbar);
 
+    // StatusbarInterface
     void showStatusMessage(QString _message, int _timeout = 0);
 
   private slots:
 
+    // BaseInterface
     void pluginsInitialized();
+
+    // OptionsInterface
+    void applyOptions();
+
+    // INIInterface
+    void loadIniFileOptions(INIFile& _ini);
+    void saveIniFileOptions(INIFile& _ini);
 
   public :
 
-    void init();
+    // OptionsInterface
+    bool initializeOptionsWidget(QWidget*& _widget);
 
+    // BaseInterface
     QString name() { return (QString("Color Plugin")); };
     QString description( ) { return (QString("Sets the Default colors e.g. for slides, paper,... snapshots")); };
+
+    /// Constructor
+    ColorPlugin();
+
+
+  //===========================================================================
+  /** @name Show Colors in Options and Store them in INI files
+    * @{ */
+  //===========================================================================
+
+  private:
+    /// the options widget to set colors
+    ColorOptions* optionsWidget_;
+
+    /// generate an icon from a given color
+    QIcon generateIcon(QRgb _color);
+
+    /// generate an icon from a given color
+    QIcon generateIcon(OpenMesh::Vec4f _color);
+
+    /// get the color from a given icon
+    QRgb getIconColor(QIcon _icon);
+
+    /// set the color of a color vector to the one in the given icon
+    void setNewColor(QIcon _icon, OpenMesh::Vec4f& _color);
+
+    /// convert betweeen uint and Vec4f
+    void convertColor(uint _color, OpenMesh::Vec4f& _vector);
+
+    /// convert betweeen uint and Vec4f
+    uint convertColor(OpenMesh::Vec4f& _vector);
+
+    //internal storage of colors
+    OpenMesh::Vec4f backgroundColorDefault_, backgroundColorPaper_, backgroundColorPresentation_;
+    OpenMesh::Vec4f baseColorDefault_, baseColorPaper_, baseColorPresentation_;
+    OpenMesh::Vec4f materialColorDefault_, materialColorPaper_, materialColorPresentation_;
+
+    double lineWidthDefault_, lineWidthPaper_, lineWidthPresentation_;
+
+  private slots:
+    /// display a color dialog
+    void getColorDialog();
+
+  /** @} */
 
   //===========================================================================
   /** @name Scriptable functions to set colors
@@ -89,7 +156,7 @@ Q_INTERFACES(StatusbarInterface)
     /**
      * Applies colors for presentations to the scene
      */
-    void setPowerpointColor();
+    void setPresentationColor();
 
     /**
      * Applies colors for papers
@@ -100,6 +167,8 @@ Q_INTERFACES(StatusbarInterface)
      * Sets the background color of the scene
     */
     void setBackgroundColor( Vector _color );
+
+  /** @} */
 
   //===========================================================================
   /** @name Other scriptable functions
