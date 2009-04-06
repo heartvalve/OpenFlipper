6274dfd5f2f30589b06318b34f2a7b85766db094
moebius <moebius@informatik.rwth-aachen.de>
2009-04-06 16:57:25 +0000

    Nothing

--- ACG/Scenegraph/BaseNode.cc
+++ ACG/Scenegraph/BaseNode.cc
@@ -12,12 +12,12 @@
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  OpenFlipper is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
 //
@@ -51,10 +51,10 @@ namespace ACG {
 namespace SceneGraph {
 
 
-//== IMPLEMENTATION ========================================================== 
+//== IMPLEMENTATION ==========================================================
 
 
-unsigned int BaseNode::last_id_used__ = 0; 
+unsigned int BaseNode::last_id_used__ = 0;
 
 
 //----------------------------------------------------------------------------
@@ -62,32 +62,32 @@ unsigned int BaseNode::last_id_used__ = 0;
 
 BaseNode::
 BaseNode(BaseNode* _parent, std::string _name)
-  : parent_(_parent), 
-    name_(_name), 
+  : parent_(_parent),
+    name_(_name),
     status_(Active),
     drawMode_(DrawModes::DEFAULT),
     depth_func_(GL_LESS),
     pickingEnabled_(true),
     dirty_ (false),
     traverseMode_ (BaseNode::NodeFirst)
-{  
+{
   id_ = ++last_id_used__;
   if (_parent!=0) _parent->push_back(this);
 
   DrawModes::initializeDefaultDrawModes();
 }
- 
+
 
 //----------------------------------------------------------------------------
 
 
 BaseNode::
 BaseNode(BaseNode* _parent, BaseNode* _child, std::string _name)
-  : parent_(_parent), 
-    name_(_name), 
+  : parent_(_parent),
+    name_(_name),
     status_(Active),
     drawMode_(DrawModes::DEFAULT)
-{  
+{
   assert(_parent != 0 && _child != 0);
 
   id_ = ++last_id_used__;
@@ -97,15 +97,15 @@ BaseNode(BaseNode* _parent, BaseNode* _child, std::string _name)
 
   DrawModes::initializeDefaultDrawModes();
 }
- 
+
 
 //----------------------------------------------------------------------------
 
 
-BaseNode::~BaseNode() 
+BaseNode::~BaseNode()
 {
   // remove myself from parent's children
-  if (parent_!=0) 
+  if (parent_!=0)
   {
     ChildIter me(parent_->find(this));
     assert(me != parent_->childrenEnd());
@@ -125,16 +125,16 @@ BaseNode::~BaseNode()
 void
 BaseNode::
 set_parent(BaseNode* _parent)
-{ 
+{
   if (parent_)
   {
     ChildIter me(parent_->find(this));
     if (me != parent_->childrenEnd())
       parent_->remove(me);
   }
-  
+
   parent_ = _parent;
-  
+
   if (parent_)
   {
     ChildIter me(parent_->find(this));
@@ -147,7 +147,7 @@ set_parent(BaseNode* _parent)
 //----------------------------------------------------------------------------
 
 
-void 
+void
 BaseNode::delete_subtree()
 {
   while (!children_.empty())
