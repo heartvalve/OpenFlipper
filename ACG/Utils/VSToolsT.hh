408acd6d3622a0e7ff0ef26b2e9626383b40bd14
moebius <moebius@informatik.rwth-aachen.de>
2009-05-14 06:24:41 +0000

    Added Visual Studio wrapper for isinf and isnan

---
+++ ACG/Utils/VSToolsT.hh
@@ -0,0 +1,50 @@
+//=============================================================================
+//
+// CLASS GeometryToolsT
+//
+//
+// Author:  Henrik Zimmer <henrik@zimmer.to>
+//
+// Version: $Revision: 1$
+// Date:    $Date: XX-XX-200X$
+//
+//=============================================================================
+
+
+#ifndef VSTOOLS_HH
+#define VSTOOLS_HH
+
+
+//== FORWARDDECLARATIONS ======================================================
+
+//== NAMESPACES ===============================================================
+
+//== DEFINITION =========================================================
+
+/** These functions are required for Visual Studio to work around missing 
+    functions. Basic equivalent functions for doulbe exist in the float 
+    header but are named different. So this wrapper makes them standard compatible.
+    */
+#ifdef WIN32
+ #include <float.h>
+
+ namespace std {
+
+   inline bool isnan(double x)
+   {
+     return _isnan(x);
+   } 
+
+   inline bool isinf(double x)
+   {
+     return !_finite(x);
+   } 
+
+  }
+#endif
+
+
+//=============================================================================
+#endif // VSTOOLS_HH defined
+//=============================================================================
+
