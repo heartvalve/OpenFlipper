75e86d80c9b2e79787c09726fcad38f1d289e18b
moebius <moebius@informatik.rwth-aachen.de>
2009-05-20 15:26:31 +0000

    Updates for glew finder

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -19,12 +19,13 @@ if( WIN32 )
    if( MSVC71 )
        set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ .NET\ 2003/Vc7" )
    endif( MSVC71 )
-   FIND_PATH( GLEW_INCLUDE_DIR gl/glew.h gl/wglew.h
-              PATHS c:/glew/include ${COMPILER_PATH}/PlatformSDK/Include )
+   FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
+		  PATH_SUFFIXES gl/ GL/ 
+              PATHS "c:/glew/include" "c:/libs/glew/include" ${COMPILER_PATH}/PlatformSDK/Include )
    SET( GLEW_NAMES glew32 )
    FIND_LIBRARY( GLEW_LIBRARY
                  NAMES ${GLEW_NAMES}
-                 PATHS c:/glew/lib ${COMPILER_PATH}/PlatformSDK/Lib )
+                 PATHS "c:/glew/lib"  "c:/libs/glew/lib" ${COMPILER_PATH}/PlatformSDK/Lib )
 else( WIN32 )
    FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
               PATHS /usr/local/include /usr/include
