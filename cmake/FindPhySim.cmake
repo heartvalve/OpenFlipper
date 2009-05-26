73a819f79141aac1cbeb0b5585e2099d8e109f30
moebius <moebius@informatik.rwth-aachen.de>
2009-05-26 21:27:40 +0000

    Added findPhysim

---
+++ cmake/FindPhySim.cmake
@@ -0,0 +1,28 @@
+# - Try to find PHYSIM
+# Once done this will define
+#  
+#  PHYSIM_FOUND        - system has PHYSIM
+#  PHYSIM_INCLUDE_DIR  - the PHYSIM include directory
+#  PHYSIM_LIBRARY_DIR  - where the libraries are
+#  PHYSIM_LIBRARY      - Link these to use PHYSIM
+#   
+
+IF (PHYSIM_INCLUDE_DIR)
+  # Already in cache, be silent
+  SET(PHYSIM_FIND_QUIETLY TRUE)
+ENDIF (PHYSIM_INCLUDE_DIR)
+
+FIND_PATH( PHYSIM_INCLUDE_DIR Math/PrincipalComponentAnalysis.hh
+           PATHS "${CMAKE_SOURCE_DIR}/PhySim" )
+
+MESSAGE(STATUS "${CMAKE_SOURCE_DIR}")
+
+IF (PHYSIM_INCLUDE_DIR)
+  SET(PHYSIM_FOUND TRUE)
+  SET( PHYSIM_LIBRARY_DIR "${ACG_PROJECT_LIBDIR}" )
+  SET( PHYSIM_LIBRARY "PhySim")
+ELSE (PHYSIM_INCLUDE_DIR)
+  SET( PHYSIM_FOUND FALSE )
+  SET( PHYSIM_LIBRARY_DIR )
+ENDIF (PHYSIM_INCLUDE_DIR)
+
