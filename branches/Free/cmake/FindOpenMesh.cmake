ab6e90066437f46282dbeb99fffa49c68482bc55
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2009-05-27 06:16:24 +0000

    Added FindOpenMesh to global cmake

---
+++ branches/Free/cmake/FindOpenMesh.cmake
@@ -0,0 +1,29 @@
+# - Try to findOPENMESH
+# Once done this will define
+#  
+# OPENMESH_FOUND           - system has OPENMESH
+# OPENMESH_INCLUDE_DIR - theOPENMESH include directory
+# OPENMESH_LIBRARY         - Link these to use OPENMESH
+# OPENMESH_LIBRARY_DIR  - Library DIR of OPENMESH
+#   
+
+IF (OPENMESH_INCLUDE_DIRS)
+  # Already in cache, be silent
+  SET(OPENMESH_FIND_QUIETLY TRUE)
+ENDIF (OPENMESH_INCLUDE_DIRS)
+
+FIND_PATH(OPENMESH_INCLUDE_DIR OpenMesh/Core/Mesh/PolyMeshT.hh
+	  PATHS /usr/local/include 
+                /usr/include 
+                "${CMAKE_SOURCE_DIR}/OpenMesh/src"
+                "${CMAKE_SOURCE_DIR}/../OpenMesh/src"
+                )
+
+IF (OPENMESH_INCLUDE_DIR )
+   SET(OPENMESH_FOUND TRUE)
+   SET(OPENMESH_LIBRARY_DIR "${OPENMESH_INCLUDE_DIR}../lib")
+   SET(OPENMESH_LIBRARY "OpenMeshCore OpenMeshTools")
+ELSE (OPENMESH_INCLUDE_DIR)
+   SET(OPENMESH_FOUND FALSE )
+ENDIF (OPENMESH_INCLUDE_DIR )
+
