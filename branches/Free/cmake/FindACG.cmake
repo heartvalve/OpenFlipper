233a70790b8b5205189d68357fe36af8dddfacfe
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2009-05-27 06:43:38 +0000

    Update for OpenMesh finder and ACG finder included ... TODO: Make ACG build standalone

---
+++ branches/Free/cmake/FindACG.cmake
@@ -0,0 +1,31 @@
+# - Try to find ACG
+# Once done this will define
+#  
+# ACG_FOUND           - system has ACG
+# ACG_INCLUDE_DIR - theACG include directory
+# ACG_LIBRARY         - Link these to use ACG
+# ACG_LIBRARY_DIR  - Library DIR of ACG
+#   
+
+IF (ACG_INCLUDE_DIRS)
+  # Already in cache, be silent
+  SET(ACG_FIND_QUIETLY TRUE)
+ENDIF (ACG_INCLUDE_DIRS)
+
+FIND_PATH(ACG_INCLUDE_DIR QtWidgets/QtBaseViewer.hh
+	  PATHS /usr/local/include 
+                /usr/include 
+                "${CMAKE_SOURCE_DIR}/ACG"
+                "${CMAKE_SOURCE_DIR}/../ACG"
+                )
+
+IF (ACG_INCLUDE_DIR )
+   MESSAGE(STATUS blubb)
+   SET(ACG_FOUND TRUE)
+   # TODO: Set correct library path!!
+   #SET(ACG_LIBRARY_DIR "${ACG_INCLUDE_DIR}/../")
+   #SET(ACG_LIBRARY "OpenMeshCore OpenMeshTools")
+ELSE (ACG_INCLUDE_DIR)
+   SET(ACG_FOUND FALSE )
+ENDIF (ACG_INCLUDE_DIR )
+
