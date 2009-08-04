4742adb617157f5f42dd3e93e0b7054edf4c928a
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2009-08-04 11:15:54 +0000

    Refactor some PluginLib translation block

---
+++ branches/Free/OpenFlipper/PluginLib/translations/PluginLib_de_DE.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name></name>
+    <message>
+        <location filename="../../BasePlugin/MenuInterface.hh" line="55"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../BasePlugin/MenuInterface.hh" line="58"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../BasePlugin/MenuInterface.hh" line="61"/>
+        <source>&amp;Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
