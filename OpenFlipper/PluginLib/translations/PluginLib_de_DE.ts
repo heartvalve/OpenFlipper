4e20b42dd44d7c3b2e1b48fcd6524aec9dd14149
moebius <moebius@informatik.rwth-aachen.de>
2009-08-05 07:36:27 +0000

    Updated PluginLib translations

--- OpenFlipper/PluginLib/translations/PluginLib_de_DE.ts
+++ OpenFlipper/PluginLib/translations/PluginLib_de_DE.ts
@@ -16,18 +16,49 @@
         <translation type="obsolete">Extras</translation>
     </message>
     <message>
-        <location filename="../../BasePlugin/MenuInterface.hh" line="55"/>
+        <location filename="../../BasePlugin/MenuInterface.hh" line="66"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../BasePlugin/MenuInterface.hh" line="58"/>
+        <location filename="../../BasePlugin/MenuInterface.hh" line="69"/>
         <source>View</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../BasePlugin/MenuInterface.hh" line="61"/>
+        <location filename="../../BasePlugin/MenuInterface.hh" line="72"/>
         <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>glViewer</name>
+    <message>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="1246"/>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="1247"/>
+        <source>Translate along &lt;b&gt;z-axis&lt;/b&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="1254"/>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="1255"/>
+        <source>Rotate around &lt;b&gt;y-axis&lt;/b&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="1262"/>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="1263"/>
+        <source>Rotate around &lt;b&gt;x-axis&lt;/b&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="2045"/>
+        <source>snapshot: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../widgets/glWidget/QtBaseViewer.cc" line="2049"/>
+        <source>could not save snapshot to </source>
         <translation type="unfinished"></translation>
     </message>
 </context>
