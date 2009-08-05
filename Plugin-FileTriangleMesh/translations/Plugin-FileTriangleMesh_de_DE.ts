0e4eca34d7106ad6ad0662e7d50843876e5ef057
moebius <moebius@informatik.rwth-aachen.de>
2009-08-05 10:38:20 +0000

    Translations

---
+++ Plugin-FileTriangleMesh/translations/Plugin-FileTriangleMesh_de_DE.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>FileTriangleMeshPlugin</name>
+    <message>
+        <source>Unable to save (Could not get object)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saved object to </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to save </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to save (object isn&apos;t a triangle mesh)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot find object for id %1 in saveFile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot find object for id %1 in saveFile!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Load/Save Triangle Meshes Meshes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
