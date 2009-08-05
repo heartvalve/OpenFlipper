f0e77b9c70b6493176c7379203cda2e4fb28d5e8
moebius <moebius@informatik.rwth-aachen.de>
2009-08-05 08:25:07 +0000

    Translations for Color Plugin

---
+++ Plugin-Color/translations/Plugin-Color_de_DE.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>colorOptions</name>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="228"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="229"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="237"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="245"/>
+        <source>Background Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="231"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="239"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="247"/>
+        <source>Base Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="233"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="241"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="249"/>
+        <source>Material Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="235"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="243"/>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="251"/>
+        <source>Line Width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="236"/>
+        <source>Default Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="244"/>
+        <source>Paper Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="252"/>
+        <source>Presentation Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
