3160e0e3c3ccebaa2abef16e1db75e4823b99dee
moebius <moebius@informatik.rwth-aachen.de>
2009-08-05 09:24:45 +0000

    Updated translations for Color Plugin (now complete)

--- Plugin-Color/translations/Plugin-Color_de_DE.ts
+++ Plugin-Color/translations/Plugin-Color_de_DE.ts
@@ -2,52 +2,135 @@
 <!DOCTYPE TS>
 <TS version="2.0">
 <context>
+    <name>ColorPlugin</name>
+    <message>
+        <location filename="../ColorPlugin.cc" line="91"/>
+        <source>&amp;Color schemes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="101"/>
+        <source>&amp;Default colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="102"/>
+        <source>Set Default Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="103"/>
+        <source>Set colors to default colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="109"/>
+        <source>&amp;Presentation colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="110"/>
+        <source>Set Presentation Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="111"/>
+        <source>Set colors to presentation colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="117"/>
+        <source>&amp;Paper colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="118"/>
+        <source>Set Paper Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="119"/>
+        <source>Set colors to colors for papers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="129"/>
+        <source>Color Toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="150"/>
+        <source>Set to default Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="193"/>
+        <source>Set to Presentation Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="236"/>
+        <source>Set to Paper Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.cc" line="276"/>
+        <source>Set background color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ColorPlugin.hh" line="112"/>
+        <source>Sets the Default colors e.g. for slides, paper,... snapshots</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>colorOptions</name>
     <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="228"/>
+        <location filename="../colorOptions.ui" line="13"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="229"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="237"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="245"/>
-        <source>Background Color:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="231"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="239"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="247"/>
-        <source>Base Color:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="233"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="241"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="249"/>
-        <source>Material Color:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="235"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="243"/>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="251"/>
-        <source>Line Width:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="236"/>
+        <location filename="../colorOptions.ui" line="23"/>
         <source>Default Colors</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="244"/>
+        <location filename="../colorOptions.ui" line="29"/>
+        <location filename="../colorOptions.ui" line="88"/>
+        <location filename="../colorOptions.ui" line="147"/>
+        <source>Background Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../colorOptions.ui" line="43"/>
+        <location filename="../colorOptions.ui" line="102"/>
+        <location filename="../colorOptions.ui" line="161"/>
+        <source>Base Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../colorOptions.ui" line="57"/>
+        <location filename="../colorOptions.ui" line="116"/>
+        <location filename="../colorOptions.ui" line="175"/>
+        <source>Material Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../colorOptions.ui" line="71"/>
+        <location filename="../colorOptions.ui" line="130"/>
+        <location filename="../colorOptions.ui" line="189"/>
+        <source>Line Width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../colorOptions.ui" line="82"/>
         <source>Paper Colors</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../build-release/Plugin-Color/ui_colorOptions.hh" line="252"/>
+        <location filename="../colorOptions.ui" line="141"/>
         <source>Presentation Colors</source>
         <translation type="unfinished"></translation>
     </message>
