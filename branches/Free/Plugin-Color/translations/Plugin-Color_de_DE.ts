7b02e13df5b3daafd2c25fb5e1e22830540a0f85
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2009-08-05 09:41:11 +0000

    Translations

--- branches/Free/Plugin-Color/translations/Plugin-Color_de_DE.ts
+++ branches/Free/Plugin-Color/translations/Plugin-Color_de_DE.ts
@@ -1,87 +1,87 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de_DE" sourcelanguage="en">
 <context>
     <name>ColorPlugin</name>
     <message>
         <location filename="../ColorPlugin.cc" line="91"/>
         <source>&amp;Color schemes</source>
-        <translation type="unfinished"></translation>
+        <translation>Farb Schemata</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="101"/>
         <source>&amp;Default colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Standard Farben</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="102"/>
         <source>Set Default Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze standard Farben</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="103"/>
         <source>Set colors to default colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Farben auf Standard zurück</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="109"/>
         <source>&amp;Presentation colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Präsentations Farben</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="110"/>
         <source>Set Presentation Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Präsentations Farben</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="111"/>
         <source>Set colors to presentation colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Farben auf Präsentations Schema</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="117"/>
         <source>&amp;Paper colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Paper Farben</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="118"/>
         <source>Set Paper Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Farben auf Paper Schema</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="119"/>
         <source>Set colors to colors for papers</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Farben auf Paper Schema</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="129"/>
         <source>Color Toolbar</source>
-        <translation type="unfinished"></translation>
+        <translation>Farb Menüleiste</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="150"/>
         <source>Set to default Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Farben auf Standard zurück</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="193"/>
         <source>Set to Presentation Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Präsentations Farben</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="236"/>
         <source>Set to Paper Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Farben auf Paper Schema</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.cc" line="276"/>
         <source>Set background color</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Hintergrund Farbe</translation>
     </message>
     <message>
         <location filename="../ColorPlugin.hh" line="112"/>
         <source>Sets the Default colors e.g. for slides, paper,... snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Setzt die Standard Farben zum Beispiel für Folien, Paper, ... Bildschirmfotos</translation>
     </message>
 </context>
 <context>
@@ -89,50 +89,50 @@
     <message>
         <location filename="../colorOptions.ui" line="13"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
     <message>
         <location filename="../colorOptions.ui" line="23"/>
         <source>Default Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Standard Farben</translation>
     </message>
     <message>
         <location filename="../colorOptions.ui" line="29"/>
         <location filename="../colorOptions.ui" line="88"/>
         <location filename="../colorOptions.ui" line="147"/>
         <source>Background Color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Hintergrund Farbe</translation>
     </message>
     <message>
         <location filename="../colorOptions.ui" line="43"/>
         <location filename="../colorOptions.ui" line="102"/>
         <location filename="../colorOptions.ui" line="161"/>
         <source>Base Color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Grund Farbe</translation>
     </message>
     <message>
         <location filename="../colorOptions.ui" line="57"/>
         <location filename="../colorOptions.ui" line="116"/>
         <location filename="../colorOptions.ui" line="175"/>
         <source>Material Color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Material Farbe</translation>
     </message>
     <message>
         <location filename="../colorOptions.ui" line="71"/>
         <location filename="../colorOptions.ui" line="130"/>
         <location filename="../colorOptions.ui" line="189"/>
         <source>Line Width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Linien Breite</translation>
     </message>
     <message>
         <location filename="../colorOptions.ui" line="82"/>
         <source>Paper Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Paper Farben</translation>
     </message>
     <message>
         <location filename="../colorOptions.ui" line="141"/>
         <source>Presentation Colors</source>
-        <translation type="unfinished"></translation>
+        <translation>Präsentations Farben</translation>
     </message>
 </context>
 </TS>
