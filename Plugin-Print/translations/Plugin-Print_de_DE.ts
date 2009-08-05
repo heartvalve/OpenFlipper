611738ca27419f2aba477e372281c305f9f7231f
moebius <moebius@informatik.rwth-aachen.de>
2009-08-05 14:33:59 +0000

    Translation

--- Plugin-Print/translations/Plugin-Print_de_DE.ts
+++ Plugin-Print/translations/Plugin-Print_de_DE.ts
@@ -1,28 +1,28 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de_DE" sourcelanguage="en">
 <context>
     <name>PrintPlugin</name>
     <message>
         <location filename="../PrintPlugin.cc" line="56"/>
         <source>&amp;Printing</source>
-        <translation type="unfinished"></translation>
+        <translation>Drucken</translation>
     </message>
     <message>
         <location filename="../PrintPlugin.cc" line="62"/>
         <source>&amp;Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Drucken</translation>
     </message>
     <message>
         <location filename="../PrintPlugin.cc" line="63"/>
         <location filename="../PrintPlugin.hh" line="72"/>
         <source>Print the current view</source>
-        <translation type="unfinished"></translation>
+        <translation>Drucke die aktuelle Ansicht</translation>
     </message>
     <message>
         <location filename="../PrintPlugin.cc" line="76"/>
         <source>Print Current View</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuelle Ansicht drucken</translation>
     </message>
 </context>
 </TS>
