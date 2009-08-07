1d704b93e451ac985cfe0b84cdaf237cd8442333
moebius <moebius@informatik.rwth-aachen.de>
2009-08-07 05:40:08 +0000

    Translations

--- OpenFlipper/PluginLib/translations/PluginLib_de_DE.ts
+++ OpenFlipper/PluginLib/translations/PluginLib_de_DE.ts
@@ -18,17 +18,71 @@
     <message>
         <location filename="../../BasePlugin/MenuInterface.hh" line="66"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei</translation>
     </message>
     <message>
         <location filename="../../BasePlugin/MenuInterface.hh" line="69"/>
         <source>View</source>
-        <translation type="unfinished"></translation>
+        <translation>Ansicht</translation>
     </message>
     <message>
         <location filename="../../BasePlugin/MenuInterface.hh" line="72"/>
         <source>Tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Zubehör</translation>
+    </message>
+</context>
+<context>
+    <name>Types</name>
+    <message>
+        <location filename="../../common/Types.cc" line="121"/>
+        <source>Unknown</source>
+        <translation>Unbekannt</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="125"/>
+        <source>Group</source>
+        <translation>Gruppe</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="129"/>
+        <source>Triangle Mesh</source>
+        <translation>Dreiecks Netz</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="133"/>
+        <source>Poly Mesh</source>
+        <translation>Polygonales Netz</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="137"/>
+        <source>B-Spline Curve</source>
+        <translation>B-Spline Kurve</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="141"/>
+        <source>B_Spline Surface</source>
+        <translation>B-Spline Fläche</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="145"/>
+        <source>Skeleton</source>
+        <translation>Skelett</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="149"/>
+        <source>GIS Data</source>
+        <translation>GIS Daten</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="153"/>
+        <source>All</source>
+        <translation>Alle</translation>
+    </message>
+    <message>
+        <location filename="../../common/Types.cc" line="275"/>
+        <location filename="../../common/Types.cc" line="289"/>
+        <source>Unknown Type</source>
+        <translation>Unbekannter Typ</translation>
     </message>
 </context>
 <context>
