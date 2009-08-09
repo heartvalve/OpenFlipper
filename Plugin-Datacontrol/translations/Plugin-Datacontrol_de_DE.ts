b602e0b1c9afb65f02a87a5829dfc492df826471
moebius <moebius@informatik.rwth-aachen.de>
2009-08-09 12:41:57 +0000

    Translations

--- Plugin-Datacontrol/translations/Plugin-Datacontrol_de_DE.ts
+++ Plugin-Datacontrol/translations/Plugin-Datacontrol_de_DE.ts
@@ -1,78 +1,78 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de_DE" sourcelanguage="en">
 <context>
     <name>DataControlPlugin</name>
     <message>
         <location filename="../ContextMenuDataControl.cc" line="107"/>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Quelle</translation>
     </message>
     <message>
         <location filename="../ContextMenuDataControl.cc" line="110"/>
         <source>Target</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziel</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.cc" line="86"/>
         <source>&amp;Hide</source>
-        <translation type="unfinished"></translation>
+        <translation>Verstecken</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.cc" line="87"/>
         <source>Hide object</source>
-        <translation type="unfinished"></translation>
+        <translation>Objekt verstecken</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.cc" line="92"/>
         <source>&amp;target</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziel</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.cc" line="94"/>
         <source>Set object as target</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Objekt als Ziel</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.cc" line="99"/>
         <source>&amp;source</source>
-        <translation type="unfinished"></translation>
+        <translation>Quelle</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.cc" line="101"/>
         <source>Set object as source</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Objekt als Quelle</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.cc" line="108"/>
         <location filename="../Popup.cc" line="312"/>
         <source>Material Properties</source>
-        <translation type="unfinished"></translation>
+        <translation>MAterial Eigenschaften</translation>
     </message>
     <message>
         <location filename="../DataControlPlugin.hh" line="133"/>
         <source>Manages Data Objects</source>
-        <translation type="unfinished"></translation>
+        <translation>Verwaltet Objekte</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="55"/>
         <source>Returns the id of an object with given name.</source>
-        <translation type="unfinished"></translation>
+        <translation>Liefert über den Namen eines Objektes seine Nummer.</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="56"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Name</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="56"/>
         <source>Name of an object</source>
-        <translation type="unfinished"></translation>
+        <translation>Name des Objektes</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="58"/>
         <source>Returns the name of an object with given id.</source>
-        <translation type="unfinished"></translation>
+        <translation>Liefert über die Nummer eines Objektes seinen Namen.</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="59"/>
@@ -81,34 +81,34 @@
         <location filename="../DataControlPluginScripting.cc" line="87"/>
         <location filename="../DataControlPluginScripting.cc" line="90"/>
         <source>objectId</source>
-        <translation type="unfinished"></translation>
+        <translation>Objekt Nummer</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="59"/>
         <location filename="../DataControlPluginScripting.cc" line="62"/>
         <location filename="../DataControlPluginScripting.cc" line="65"/>
         <source>ID of an object</source>
-        <translation type="unfinished"></translation>
+        <translation>Nummer eines Objektes</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="61"/>
         <source>Hide object with the given id.</source>
-        <translation type="unfinished"></translation>
+        <translation>Verstecket das Objekt mit der gegebenen Nummer.</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="64"/>
         <source>Show object with the given id.</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeigt das Objekt mit der angegebenen Nummer.</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="67"/>
         <source>Set given object as target.</source>
-        <translation type="unfinished"></translation>
+        <translation>Setzt das angegebene Objekt als Ziel.</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="68"/>
         <source>ObjectId,Target</source>
-        <translation type="unfinished"></translation>
+        <translation>Objekt Nummer,Ziel</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="69"/>
@@ -250,102 +250,102 @@
     <message>
         <location filename="../Popup.cc" line="220"/>
         <source>newGroup</source>
-        <translation type="unfinished"></translation>
+        <translation>Neue Gruppe</translation>
     </message>
     <message>
         <location filename="../DataControlPluginScripting.cc" line="325"/>
         <location filename="../Popup.cc" line="221"/>
         <source>newGroup </source>
-        <translation type="unfinished"></translation>
+        <translation>Neue Gruppe</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="284"/>
         <location filename="../Popup.cc" line="315"/>
         <source>Group</source>
-        <translation type="unfinished"></translation>
+        <translation>Gruppe</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="286"/>
         <location filename="../Popup.cc" line="299"/>
         <location filename="../Popup.cc" line="317"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Entfernen</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="290"/>
         <source>Zoom to objects</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoom auf Objekt</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="293"/>
         <source>Ungroup</source>
-        <translation type="unfinished"></translation>
+        <translation>Grupierung aufheben</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="295"/>
         <location filename="../Popup.cc" line="309"/>
         <location filename="../Popup.cc" line="451"/>
         <source>Rename</source>
-        <translation type="unfinished"></translation>
+        <translation>Umbenennnen</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="303"/>
         <source>Zoom to object</source>
-        <translation type="unfinished"></translation>
+        <translation>Zoom auf Objekt</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="306"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieren</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="360"/>
         <source>Show all</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeige Alle</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="365"/>
         <source>Show all of type &apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeige Alle vom Typ &apos;</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="370"/>
         <source>Hide all</source>
-        <translation type="unfinished"></translation>
+        <translation>Verstecke Alle</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="375"/>
         <source>Hide all of type &apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>VErstecke Alle vom Typ &apos;</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="382"/>
         <location filename="../Popup.cc" line="404"/>
         <source>Select all</source>
-        <translation type="unfinished"></translation>
+        <translation>Selektiere Alle</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="387"/>
         <location filename="../Popup.cc" line="409"/>
         <source>Select all of type &apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Selektiere Alle vom Typ &apos;</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="392"/>
         <location filename="../Popup.cc" line="414"/>
         <source>Deselect all</source>
-        <translation type="unfinished"></translation>
+        <translation>Hebe Selektion für Alle auf</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="397"/>
         <location filename="../Popup.cc" line="419"/>
         <source>Deselect all of type &apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Hebe Selektion für Objekte vom Typ &apos;</translation>
     </message>
     <message>
         <location filename="../Popup.cc" line="452"/>
         <source>Enter a new name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Geben sie einen neuen Namen ein:</translation>
     </message>
 </context>
 <context>
@@ -353,27 +353,27 @@
     <message>
         <location filename="../toolbox.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Dialog</translation>
     </message>
     <message>
         <location filename="../toolbox.ui" line="42"/>
         <source>Object Bounding Box</source>
-        <translation type="unfinished"></translation>
+        <translation>Objekt Bounding Box</translation>
     </message>
     <message>
         <location filename="../toolbox.ui" line="50"/>
         <source>Not selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Nicht ausgewählt</translation>
     </message>
     <message>
         <location filename="../toolbox.ui" line="57"/>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Quelle</translation>
     </message>
     <message>
         <location filename="../toolbox.ui" line="64"/>
         <source>Target</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziel</translation>
     </message>
 </context>
 </TS>
