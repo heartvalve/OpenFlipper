344298ff238b41427bb16742f2b4170bb9181e50
habbecke <martin.habbecke@gmail.com>
2009-09-02 07:28:25 +0000

    * fixed library path and list

--- cmake/FindOpenMesh.cmake
+++ cmake/FindOpenMesh.cmake
@@ -17,12 +17,13 @@ FIND_PATH(OPENMESH_INCLUDE_DIR OpenMesh/Core/Mesh/PolyMeshT.hh
                 /usr/include 
                 "${CMAKE_SOURCE_DIR}/OpenMesh/src"
                 "${CMAKE_SOURCE_DIR}/../OpenMesh/src"
+                /ACG/acgdev/gcc-4.0-x86_64/OM2/OpenMesh-2.0/installed/include
                 )
 
 IF (OPENMESH_INCLUDE_DIR )
    SET(OPENMESH_FOUND TRUE)
-   SET(OPENMESH_LIBRARY_DIR "${OPENMESH_INCLUDE_DIR}/../lib")
-   SET(OPENMESH_LIBRARY "OpenMeshCore OpenMeshTools")
+   SET(OPENMESH_LIBRARY_DIR "${OPENMESH_INCLUDE_DIR}/../lib/OpenMesh")
+   SET(OPENMESH_LIBRARY "OpenMeshCore;OpenMeshTools")
 ELSE (OPENMESH_INCLUDE_DIR)
    SET(OPENMESH_FOUND FALSE )
 ENDIF (OPENMESH_INCLUDE_DIR )
