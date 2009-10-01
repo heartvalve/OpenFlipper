fcfcddef10eaf1d9ff6f44d1f3f36a643f6c7324
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2009-10-01 12:28:19 +0000

    Added Icon for view mode all

--- branches/Free/OpenFlipper/Core/Core.cc
+++ branches/Free/OpenFlipper/Core/Core.cc
@@ -137,7 +137,7 @@ Core() :
    // Add ViewMode All
   ViewMode* vm = new ViewMode();
   vm->name = "All";
-  vm->icon = "Unknown.png";
+  vm->icon = "viewmode_all.png";
   vm->custom = false;
   vm->visibleToolboxes = QStringList();
 
