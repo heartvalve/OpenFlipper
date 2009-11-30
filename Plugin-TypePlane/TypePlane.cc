49550177137bb1e794dc05cc30087f53cf84533c
moebius <moebius@informatik.rwth-aachen.de>
2009-11-30 13:26:11 +0000

    Added TypePlane Plugin

---
+++ Plugin-TypePlane/TypePlane.cc
@@ -0,0 +1,61 @@
+/*===========================================================================*\
+ *                                                                           *
+ *                              OpenFlipper                                  *
+ *      Copyright (C) 2001-2009 by Computer Graphics Group, RWTH Aachen      *
+ *                           www.openflipper.org                             *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *  This file is part of OpenFlipper.                                        *
+ *                                                                           *
+ *  OpenFlipper is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
+ *                                                                           *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
+ *                                                                           *
+ *  OpenFlipper is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with OpenFlipper. If not,                                  *
+ *  see <http://www.gnu.org/licenses/>.                                      *
+ *                                                                           *
+\*===========================================================================*/
+
+/*===========================================================================*\
+ *                                                                           *
+ *   $Revision: 6753 $                                                       *
+ *   $Author: moebius $                                                      *
+ *   $Date: 2009-08-05 12:38:20 +0200 (Mi, 05. Aug 2009) $                   *
+ *                                                                           *
+\*===========================================================================*/
+
+
+
+
+#include "TypePlane.hh"
+
+
+
+TypePlanePlugin::TypePlanePlugin() {
+  
+}
+
+bool TypePlanePlugin::registerType() {
+  addDataType("Plane",tr("Plane"));
+  setTypeIcon( "Plane", "PlaneType.png");
+  return true;
+}
+
+Q_EXPORT_PLUGIN2( typeplaneplugin , TypePlanePlugin );
+
