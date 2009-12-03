fc9b40411bb53d306d24410d855f711dcf5db2dc
moebius <moebius@informatik.rwth-aachen.de>
2009-12-03 09:07:45 +0000

    Added libSVM Finder

---
+++ cmake/FindSVM.cmake
@@ -0,0 +1,27 @@
+# - Try to find SVM
+# Once done this will define
+#  
+#  SVM_FOUND        - system has SVM
+#  SVM_INCLUDE_DIR  - the SVM include directory
+#  SVM_LIBRARY      - Link these to use SVM
+#   
+
+IF (SVM_INCLUDE_DIRS)
+  # Already in cache, be silent
+  SET(SVM_FIND_QUIETLY TRUE)
+ENDIF (SVM_INCLUDE_DIRS)
+
+FIND_PATH( SVM_INCLUDE_DIR libsvm/svm.h
+           PATHS "/usr/include/libsvm-2.0" )
+
+ FIND_LIBRARY( SVM_LIBRARY
+               NAMES svm
+               PATHS /lib /usr/lib /usr/local/lib )
+
+
+IF (SVM_INCLUDE_DIR AND SVM_LIBRARY)
+  SET(SVM_FOUND TRUE)
+ELSE (SVM_INCLUDE_DIR AND SVM_LIBRARY)
+  SET( SVM_FOUND FALSE )
+ENDIF (SVM_INCLUDE_DIR AND SVM_LIBRARY)
+
