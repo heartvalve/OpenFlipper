604cf13c3817448c45abde4b5b8c21a66e9baf3f
moebius <moebius@informatik.rwth-aachen.de>
2009-12-17 13:17:58 +0000

    Uebersetzung

--- Plugin-Decimater/translations/Plugin-Decimater_de_DE.ts
+++ Plugin-Decimater/translations/Plugin-Decimater_de_DE.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.0" language="de_DE">
 <context>
     <name>DecimaterPlugin</name>
     <message>
         <location filename="../DecimaterPlugin.cc" line="104"/>
         <source>Decimate a given object</source>
-        <translation type="unfinished"></translation>
+        <translation>Objekt dezimieren</translation>
     </message>
     <message>
         <location filename="../DecimaterPlugin.cc" line="105"/>
@@ -47,7 +47,7 @@
     <message>
         <location filename="../DecimaterPlugin.hh" line="117"/>
         <source>Mesh Decimation ...</source>
-        <translation type="unfinished"></translation>
+        <translation>Mesh Dezimierung ...</translation>
     </message>
 </context>
 <context>
@@ -55,29 +55,29 @@
     <message>
         <location filename="../decimaterToolbar.ui" line="14"/>
         <source>Form</source>
-        <translation type="unfinished"></translation>
+        <translation>Box</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="20"/>
         <source>Constraints</source>
-        <translation type="unfinished"></translation>
+        <translation>Einschränkungen</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="28"/>
         <source>Distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Abstand</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="51"/>
         <location filename="../decimaterToolbar.ui" line="54"/>
         <location filename="../decimaterToolbar.ui" line="72"/>
         <source>Restrict Normal Deviation</source>
-        <translation type="unfinished"></translation>
+        <translation>Normalen Abweichung begrenzen</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="57"/>
         <source>Normal Dev.</source>
-        <translation type="unfinished"></translation>
+        <translation>Normalen Abw.</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="64"/>
@@ -87,7 +87,12 @@ p, li { white-space: pre-wrap; }
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Maximal normal deviation between &lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;original normal and normals after &lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;decimation step defined in degrees.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Maximale Normalen Abweichung zwischen &lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;originaler Normale und der Normale nach &lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;dem dezimierungs Schritt in Grad.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="91"/>
@@ -98,37 +103,42 @@ p, li { white-space: pre-wrap; }
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;original normal and normals after &lt;/p&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;decimation step defined in degrees.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <comment>Maximal normal deviation between original normal and normals after decimation step defined in degrees.</comment>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Maximale Normalen Abweichung zwischen &lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;originaler Normale und der Normale nach &lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;dem dezimierungs Schritt in Grad.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="99"/>
         <source>Restrict Normal deviation</source>
-        <translation type="unfinished"></translation>
+        <translation>Normalen Abweichung begrenzen</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="115"/>
         <source>Roundness</source>
-        <translation type="unfinished"></translation>
+        <translation>Rundheit</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="163"/>
         <source>#Vertices</source>
-        <translation type="unfinished"></translation>
+        <translation>#Knoten</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="200"/>
         <source>Current number of vertices:</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktuelle Knotenzahl:</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="213"/>
         <source>&lt;not available&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Nicht verfügbar</translation>
     </message>
     <message>
         <location filename="../decimaterToolbar.ui" line="225"/>
         <source>Decimate</source>
-        <translation type="unfinished"></translation>
+        <translation>Dezimieren</translation>
     </message>
 </context>
 </TS>
