48d3290bc89b388b790afffefd6fea6729ec639c
moebius <moebius@informatik.rwth-aachen.de>
2010-01-05 16:56:33 +0000

    Added cmake option to do stl checks in debug mode

--- cmake/ACGCompiler.cmake
+++ cmake/ACGCompiler.cmake
@@ -6,10 +6,21 @@ if (UNIX)
 
   IF( NOT CMAKE_SYSTEM MATCHES "SunOS*")
 
+    # Pre initialize this variable   
+    if ( NOT STL_VECTOR_CHECKS )
+      set ( STL_VECTOR_CHECKS false CACHE BOOL "Include full stl vector checks in debug mode (This option is only used in debug Mode!)" )
+    endif ( NOT STL_VECTOR_CHECKS )
+
     set (CMAKE_CFLAGS_RELEASE "-O3 -DINCLUDE_TEMPLATES -W -Wall -Wno-unused -DNDEBUG")
     set (CMAKE_CXX_FLAGS_RELEASE "-O3 -DINCLUDE_TEMPLATES -ftemplate-depth-100 -W -Wall -Wno-unused -DNDEBUG")
-    set (CMAKE_C_FLAGS_DEBUG "-g -DINCLUDE_TEMPLATES -W -Wall -Wno-unused -DDEBUG")
-    set (CMAKE_CXX_FLAGS_DEBUG "-g -DINCLUDE_TEMPLATES -ftemplate-depth-100 -W -Wall -Wno-unused -DDEBUG")
+
+    if ( STL_VECTOR_CHECKS )
+      set (CMAKE_C_FLAGS_DEBUG "-g -DINCLUDE_TEMPLATES -W -Wall -Wno-unused -DDEBUG -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC ")
+      set (CMAKE_CXX_FLAGS_DEBUG "-g -DINCLUDE_TEMPLATES -ftemplate-depth-100 -W -Wall -Wno-unused -DDEBUG -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC")
+    else ( STL_VECTOR_CHECKS )
+      set (CMAKE_C_FLAGS_DEBUG "-g -DINCLUDE_TEMPLATES -W -Wall -Wno-unused -DDEBUG")
+      set (CMAKE_CXX_FLAGS_DEBUG "-g -DINCLUDE_TEMPLATES -ftemplate-depth-100 -W -Wall -Wno-unused -DDEBUG")
+    endif ( STL_VECTOR_CHECKS )
     
   ELSE ( NOT CMAKE_SYSTEM MATCHES "SunOS*")
 
