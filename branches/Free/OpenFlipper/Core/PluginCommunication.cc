275d95ce2b747c6d3204e355c0960c9ec9fa710a
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2010-02-03 10:58:47 +0000

    Function for object deletion

--- branches/Free/OpenFlipper/Core/PluginCommunication.cc
+++ branches/Free/OpenFlipper/Core/PluginCommunication.cc
@@ -324,6 +324,23 @@ void Core::newObject(int _objectId) {
    
 }
 
+/// This slot is called by the object manager when a object is deleted
+void Core::deletedObject(int _objectId) {
+  
+//   BaseObject* baseObject = 0;
+//   PluginFunctions::getObject(_objectId,baseObject);
+//   
+//   if ( baseObject ) {
+//     connect( baseObject, SIGNAL(visibilityChanged(int)),       this, SLOT(slotVisibilityChanged(int)),       Qt::DirectConnection) ;
+//     connect( baseObject, SIGNAL(objectSelectionChanged(int)),  this, SLOT(slotObjectSelectionChanged(int)),  Qt::DirectConnection );
+//     connect( baseObject, SIGNAL(objectPropertiesChanged(int)), this, SLOT(slotObjectPropertiesChanged(int)), Qt::DirectConnection );
+//   } else {
+//     emit log(LOGERR,tr("newObject received from objectManager with invalid id! This should not happen. The new Object will not work correctly!"));
+//   }
+//   
+  emit log(LOGERR,tr("deletedObject received from objectManager!"));
+}
+
 
 
 
