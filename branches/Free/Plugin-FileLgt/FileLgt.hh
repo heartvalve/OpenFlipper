538c72f5a18c93bfd25de3d0feb52cf6218ce373
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2010-04-12 07:01:49 +0000

    Added FileLight Plugin

---
+++ branches/Free/Plugin-FileLgt/FileLgt.hh
@@ -0,0 +1,118 @@
+/*===========================================================================*\
+*                                                                           *
+*                              OpenFlipper                                  *
+*      Copyright (C) 2001-2009 by Computer Graphics Group, RWTH Aachen      *
+*                           www.openflipper.org                             *
+*                                                                           *
+*---------------------------------------------------------------------------*
+*  This file is part of OpenFlipper.                                        *
+*                                                                           *
+*  OpenFlipper is free software: you can redistribute it and/or modify      *
+*  it under the terms of the GNU Lesser General Public License as           *
+*  published by the Free Software Foundation, either version 3 of           *
+*  the License, or (at your option) any later version with the              *
+*  following exceptions:                                                    *
+*                                                                           *
+*  If other files instantiate templates or use macros                       *
+*  or inline functions from this file, or you compile this file and         *
+*  link it with other files to produce an executable, this file does        *
+*  not by itself cause the resulting executable to be covered by the        *
+*  GNU Lesser General Public License. This exception does not however       *
+*  invalidate any other reasons why the executable file might be            *
+*  covered by the GNU Lesser General Public License.                        *
+*                                                                           *
+*  OpenFlipper is distributed in the hope that it will be useful,           *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+*  GNU Lesser General Public License for more details.                      *
+*                                                                           *
+*  You should have received a copy of the GNU LesserGeneral Public          *
+*  License along with OpenFlipper. If not,                                  *
+*  see <http://www.gnu.org/licenses/>.                                      *
+*                                                                           *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                           *
+*   $Revision: 7920 $                                                       *
+*   $Author: moebius $                                                       *
+*   $Date: 2009-12-15 10:07:03 +0100 (Di, 15. Dez 2009) $                   *
+*                                                                           *
+\*===========================================================================*/
+
+#ifndef FILELIGHTPLUGIN_HH
+#define FILELIGHTPLUGIN_HH
+
+#include <QObject>
+
+#include <OpenFlipper/common/Types.hh>
+#include <OpenFlipper/BasePlugin/BaseInterface.hh>
+#include <OpenFlipper/BasePlugin/FileInterface.hh>
+#include <OpenFlipper/BasePlugin/LoadSaveInterface.hh>
+#include <OpenFlipper/BasePlugin/LoggingInterface.hh>
+#include <OpenFlipper/BasePlugin/ScriptInterface.hh>
+#include <OpenFlipper/BasePlugin/INIInterface.hh>
+#include <ObjectTypes/Light/Light.hh>
+
+class FileLightPlugin : public QObject, BaseInterface, FileInterface, LoadSaveInterface, LoggingInterface, ScriptInterface, INIInterface
+{
+   Q_OBJECT
+   Q_INTERFACES(FileInterface)
+   Q_INTERFACES(LoadSaveInterface)
+   Q_INTERFACES(LoggingInterface)
+   Q_INTERFACES(BaseInterface)
+   Q_INTERFACES(ScriptInterface)
+   Q_INTERFACES(INIInterface)
+
+  signals:
+    void openedFile( int _id );
+    void addEmptyObject( DataType _type, int& _id);
+    void load(QString _filename, DataType _type, int& _id);
+    void save(int _id , QString _filename );
+    void log(Logtype _type, QString _message);
+    void log(QString _message);
+
+    void emptyObjectAdded( int _id );
+
+  private slots:
+
+    void fileOpened( int /*_id*/ ){};
+
+    void noguiSupported( ) {} ;
+
+    void initializePlugin();
+    
+    // Deprecated!
+    int addEmpty() { return -1; };
+
+  public :
+
+     ~FileLightPlugin() {};
+
+     QString name() { return (QString("FileLight")); };
+     QString description( ) { return (QString(tr("Load/Save Lights"))); };
+
+     DataType supportedType();
+
+     QString getSaveFilters();
+     QString getLoadFilters();
+
+     QWidget* saveOptionsWidget(QString /*_currentFilter*/) { return 0; };
+     QWidget* loadOptionsWidget(QString /*_currentFilter*/) { return 0; };
+
+  public slots:
+
+    int loadObject(QString _filename);
+
+    bool saveObject(int _id, QString _filename);
+
+    int addNewLight();
+
+    QString version() { return QString("1.0"); };
+
+  private :
+
+    QString get_unique_name(LightObject* _object);
+};
+
+#endif //FILELIGHTPLUGIN_HH
