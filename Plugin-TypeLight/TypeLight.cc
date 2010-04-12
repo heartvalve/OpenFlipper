ddb413e29a301218369401176ea8964dc945765d
moebius <moebius@informatik.rwth-aachen.de>
2010-04-12 07:02:01 +0000

    Added TypeLight Plugin

--- Plugin-TypeLight/TypeLight.cc
+++ Plugin-TypeLight/TypeLight.cc
@@ -52,19 +52,94 @@ TypeLightPlugin::TypeLightPlugin() {
 bool TypeLightPlugin::registerType() {
   addDataType("Light",tr("Light"));
   setTypeIcon( "Light", "LightType.png");
+  
+   
+  
+  
+//   int objectId1 = -1;
+//   int objectId2 = -1;
+//   emit addEmptyObject(objectId0);
+//   emit addEmptyObject(objectId1);
+//   emit addEmptyObject(objectId2);
+  
+
+  
   return true;
+}
+
+void TypeLightPlugin::pluginsInitialized(){
+  
+  // Disable the build in light management and use this plugins light handling
+  PluginFunctions::disableExaminerLightHandling();
+  
+  // Generate the default lights
+  int objectId0 = addEmpty();
+  
+  BaseObjectData* obj0(0);
+  if(PluginFunctions::getObject( objectId0, obj0))
+  {
+    LightObject* lightObject0 = PluginFunctions::lightObject(obj0);
+    LightSource* light0 = PluginFunctions::lightSource(lightObject0);
+    
+    if( light0 ) { 
+      light0->direction(ACG::Vec3d(0.0,0.0,1.0) );
+      light0->diffuseColor( ACG::Vec4f(0.7,0.7,0.7,0.0));
+      light0->specularColor(ACG::Vec4f(0.7,0.7,0.7,0.0));
+      light0->enable();
+      light0->fixedPosition(true);
+      std::cerr << "Set light 0 " << std::endl;
+    }
+    
+  }
+  
+  emit updatedObject(objectId0,UPDATE_ALL);
+  
+  // Generate the default lights
+  int objectId1 = addEmpty();
+  
+  BaseObjectData* obj1(0);
+  if(PluginFunctions::getObject( objectId1, obj1))
+  {
+    LightObject* lightObject1 = PluginFunctions::lightObject(obj1);
+    LightSource* light1 = PluginFunctions::lightSource(lightObject1);
+    
+    if( light1 ) {
+      light1->direction(ACG::Vec3d(-1.0,  1.0, 0.7) );
+      light1->diffuseColor( ACG::Vec4f(0.7,0.7,0.7,0.0));
+      light1->specularColor(ACG::Vec4f(0.7,0.7,0.7,0.0));
+      light1->enable();
+      light1->fixedPosition(true);
+    }
+  }
+  
+  emit updatedObject(objectId1,UPDATE_ALL);
+  
+  //Generate the default lights
+  int objectId2 = addEmpty();
+  
+  BaseObjectData* obj2(0);
+  if(PluginFunctions::getObject( objectId2, obj2))
+  {
+    LightObject* lightObject2 = PluginFunctions::lightObject(obj2);
+    LightSource* light2 = PluginFunctions::lightSource(lightObject2);
+    
+    if( light2 ) {
+      light2->direction(ACG::Vec3d( 1.0,  1.0, 0.7) );
+      light2->diffuseColor(ACG::Vec4f(0.7,0.7,0.7,0.0));
+      light2->specularColor(ACG::Vec4f(0.7,0.7,0.7,0.0));
+      light2->enable();
+      light2->fixedPosition(true);
+    }
+  }
+  
+  emit updatedObject(objectId2,UPDATE_ALL);
+   
 }
 
 int TypeLightPlugin::addEmpty(){
     
   // new object data struct
   LightObject * object = new LightObject();
-
-  if ( PluginFunctions::objectCount() == 1 )
-    object->target(true);
-
-  if (PluginFunctions::targetCount() == 0 )
-    object->target(true);
 
   QString name = get_unique_name(object);
 
@@ -74,6 +149,7 @@ int TypeLightPlugin::addEmpty(){
 
   object->update();
 
+  
   object->show();
 
   emit emptyObjectAdded (object->id() );
