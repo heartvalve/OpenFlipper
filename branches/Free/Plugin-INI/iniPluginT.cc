28b103d149bcf7acc35634ff1e1c2b2ed9e62a8b
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2010-04-21 08:49:22 +0000

    Write all things from materialnode in baseobjectdata

--- branches/Free/Plugin-INI/iniPluginT.cc
+++ branches/Free/Plugin-INI/iniPluginT.cc
@@ -10,46 +10,12 @@ template <class MeshObject>
 void INIPlugin::parseIniFileT(INIFile& _ini, MeshObject* _object) {
     
     if ( _object ) {
-        std::vector< QString > draw_modes;
-        if( _ini.get_entry(draw_modes, _object->name(), "MaterialDrawMode") )
-            _object->materialNode()->drawMode( listToDrawMode(draw_modes) );
-        
-        if( _ini.get_entry(draw_modes, _object->name(), "MeshDrawMode") )
-            _object->meshNode()->drawMode( listToDrawMode(draw_modes) );
-        
-        ACG::Vec4f col(0.0,0.0,0.0,0.0);
-        
-        if ( _ini.get_entryVecf( col, _object->name() , "BaseColor" ) )
-            _object->materialNode()->set_base_color(col);
-        
-        if ( _ini.get_entryVecf( col, _object->name() , "AmbientColor" ) )
-            _object->materialNode()->set_ambient_color(col);
-        
-        if ( _ini.get_entryVecf( col, _object->name() , "DiffuseColor" ) )
-            _object->materialNode()->set_diffuse_color(col);
-        
-        if ( _ini.get_entryVecf( col, _object->name() , "SpecularColor" ) )
-            _object->materialNode()->set_specular_color(col);
-        
-        double shininess;
-        if ( _ini.get_entry( shininess, _object->name() , "Shininess" ) )
-          _object->materialNode()->set_shininess(shininess);
-        
-        double reflectance;
-        if ( _ini.get_entry( reflectance, _object->name() , "Reflectance" ) ) {
-          _object->materialNode()->set_reflectance(reflectance);        
-        }
-        
-        bool visible;
-        if ( _ini.get_entry( visible, _object->name() , "Visible" ) ) {
-          _object->visible(visible);        
-        }
-        
-        int size = 1;
-        if ( _ini.get_entry( size, _object->name() , "PointSize" ) )
-            _object->materialNode()->set_point_size(size);
-        if ( _ini.get_entry( size, _object->name() , "LineWidth" ) )
-            _object->materialNode()->set_line_width(size);
+      
+      std::vector< QString > draw_modes;
+      
+      if( _ini.get_entry(draw_modes, _object->name(), "MeshDrawMode") )
+          _object->meshNode()->drawMode( listToDrawMode(draw_modes) );
+
     }
 }
 
@@ -58,38 +24,8 @@ void INIPlugin::saveIniFileT(INIFile& _ini, MeshObject* _object) {
 
     if ( _object ) {
         _ini.add_entry( _object->name() ,
-            "MaterialDrawMode" ,
-            drawModeToList( _object->materialNode()->drawMode()) );
-        _ini.add_entry( _object->name() ,
             "MeshDrawMode" ,
             drawModeToList( _object->meshNode()->drawMode()) );
-        _ini.add_entryVec( _object->name() ,
-            "BaseColor" ,
-            _object->materialNode()->base_color()) ;
-        _ini.add_entryVec( _object->name() ,
-            "AmbientColor" ,
-            _object->materialNode()->ambient_color()) ;
-        _ini.add_entryVec( _object->name() ,
-            "DiffuseColor" ,
-            _object->materialNode()->diffuse_color());
-        _ini.add_entryVec( _object->name() ,
-            "SpecularColor" ,
-            _object->materialNode()->specular_color());
-        _ini.add_entry( _object->name() ,
-            "Shininess" ,
-            _object->materialNode()->shininess());
-        _ini.add_entry( _object->name() ,
-            "Reflectance" ,
-            _object->materialNode()->reflectance());       
-        _ini.add_entry( _object->name() ,
-            "Visible" ,
-            _object->visible() );     
-        _ini.add_entry( _object->name() ,
-            "PointSize" ,
-            _object->materialNode()->point_size());
-        _ini.add_entry( _object->name() ,
-            "LineWidth" ,
-            _object->materialNode()->line_width());
     }
 }
 
