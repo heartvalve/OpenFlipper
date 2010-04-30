1c1bdab9b1e26a9e9941ba92b43fa8843c3e5965
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2010-04-30 08:38:05 +0000

    Added raw implementation of QtWidgetNode

---
+++ branches/Free/ACG/QtScenegraph/QtWidgetNode.cc
@@ -0,0 +1,103 @@
+/*===========================================================================*\
+ *                                                                           *
+ *                              OpenFlipper                                  *
+ *      Copyright (C) 2001-2009 by Computer Graphics Group, RWTH Aachen      *
+ *                           www.openflipper.org                             *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *  This file is part of OpenFlipper.                                        *
+ *                                                                           *
+ *  OpenFlipper is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
+ *                                                                           *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
+ *                                                                           *
+ *  OpenFlipper is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with OpenFlipper. If not,                                  *
+ *  see <http://www.gnu.org/licenses/>.                                      *
+ *                                                                           *
+\*===========================================================================*/
+
+/*===========================================================================*\
+ *                                                                           *
+ *   $Revision: 8876 $                                                       *
+ *   $Author: dekkers $                                                      *
+ *   $Date: 2010-04-01 14:31:02 +0200 (Do, 01. Apr 2010) $                   *
+ *                                                                           *
+\*===========================================================================*/
+
+
+
+
+//=============================================================================
+//
+//  CLASS QtWidgetNode - IMPLEMENTATION
+//
+//=============================================================================
+
+//== INCLUDES =================================================================
+
+#include "QtWidgetNode.hh"
+#include <ACG/GL/gl.hh>
+
+//== NAMESPACES ===============================================================
+
+namespace ACG {
+namespace SceneGraph {
+
+
+//== IMPLEMENTATION ========================================================== 
+
+
+void
+QtWidgetNode::
+boundingBox(Vec3d& _bbMin, Vec3d& _bbMax)
+{
+//   ConstPointIter p_it=points_.begin(), p_end=points_.end();
+//   for (; p_it!=p_end; ++p_it) {
+//     _bbMin.minimize(*p_it); 
+//     _bbMax.maximize(*p_it);
+//   }
+}
+
+
+//----------------------------------------------------------------------------
+
+  
+DrawModes::DrawMode 
+QtWidgetNode::
+availableDrawModes() const
+{
+  return ( DrawModes::SOLID_FLAT_SHADED );
+}
+
+
+//----------------------------------------------------------------------------
+
+
+void
+QtWidgetNode::
+draw(GLState& /* _state */ , DrawModes::DrawMode _drawMode)
+{
+ 
+}
+
+
+//=============================================================================
+} // namespace SceneGraph
+} // namespace ACG
+//=============================================================================
