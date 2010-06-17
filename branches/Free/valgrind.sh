81aceae072857c761e3227623860334acefe62ac
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2010-06-17 10:48:23 +0000

    Changed license Header to 2010

--- branches/Free/valgrind.sh
+++ branches/Free/valgrind.sh
@@ -1,4 +1,4 @@
-#valgrind --log-file=valgrind.log --leak-check=full ./build-debug/Build/bin/OpenFlipper -c $@
+valgrind --log-file=valgrind.log --leak-check=full ./build-debug/Build/bin/OpenFlipper -c $@
 #valgrind --tool=cachegrind --log-file=valgrind.log ./OpenFlipper/Debian_64_Debug/OpenFlipper $@
-valgrind --tool=callgrind --log-file=valgrind.log ./build-debug/Build/bin/OpenFlipper  $@
+#valgrind --tool=callgrind --log-file=valgrind.log ./build-debug/Build/bin/OpenFlipper  $@
 
