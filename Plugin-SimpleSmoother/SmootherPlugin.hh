c4116437b6f7cac83d32fcfc70c76be65453c1e1
kremer <mkremer@informatik.rwth-aachen.de>
2010-07-01 12:21:45 +0000

    Made simple smoother plugin scriptable and added backup as well as script info logging to the smoothing function.

--- Plugin-SimpleSmoother/SmootherPlugin.hh
+++ Plugin-SimpleSmoother/SmootherPlugin.hh
@@ -49,26 +49,38 @@
 #include <OpenFlipper/BasePlugin/BaseInterface.hh>
 #include <OpenFlipper/BasePlugin/ToolboxInterface.hh>
 #include <OpenFlipper/BasePlugin/LoggingInterface.hh>
+#include <OpenFlipper/BasePlugin/ScriptInterface.hh>
+#include <OpenFlipper/BasePlugin/BackupInterface.hh>
 #include <OpenFlipper/common/Types.hh>
 
-class SmootherPlugin : public QObject, BaseInterface, ToolboxInterface, LoggingInterface
+class SmootherPlugin : public QObject, BaseInterface, ToolboxInterface, LoggingInterface, ScriptInterface, BackupInterface
 {
   Q_OBJECT
   Q_INTERFACES(BaseInterface)
   Q_INTERFACES(ToolboxInterface)
   Q_INTERFACES(LoggingInterface)
+  Q_INTERFACES(ScriptInterface)
+  Q_INTERFACES(BackupInterface)
 
   signals:
     //BaseInterface
     void updateView();
     void updatedObject(int _id, const UpdateType _type);
+    void setSlotDescription(QString     _slotName,   QString     _slotDescription,
+                          QStringList _parameters, QStringList _descriptions);
 
     //LoggingInterface
     void log(Logtype _type, QString _message);
     void log(QString _message);
     
     // ToolboxInterface
-    void addToolbox( QString _name  , QWidget* _widget );   
+    void addToolbox( QString _name  , QWidget* _widget );
+    
+    // ScriptInterface
+    void scriptInfo(QString _functionName);
+    
+    // BackupInterface
+    void createBackup(int _objectId, QString _description);
 
   public:
 
@@ -86,9 +98,14 @@ class SmootherPlugin : public QObject, BaseInterface, ToolboxInterface, LoggingI
     
     void initializePlugin(); // BaseInterface
     
-
+    void pluginsInitialized(); // BaseInterface
+    
+    // Scriptable functions
    public slots:
-      QString version() { return QString("1.0"); };
+       
+    void simpleLaplace(int _iterations);
+       
+    QString version() { return QString("1.0"); };
 };
 
 #endif //SMOOTHERPLUGIN_HH
