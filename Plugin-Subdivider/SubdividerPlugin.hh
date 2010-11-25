21cef238e5eb7c560150c7a292f76dd0565b2944
moebius <moebius@informatik.rwth-aachen.de>
2010-11-25 07:32:56 +0000

    Updated license headers

--- Plugin-Subdivider/SubdividerPlugin.hh
+++ Plugin-Subdivider/SubdividerPlugin.hh
@@ -1,8 +1,49 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2010 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision$                                                       *
+*   $LastChangedBy$                                                *
+*   $Date$                     *
+*                                                                            *
+\*===========================================================================*/
+
+
 #ifndef SUBDIVIDERPLUGIN_HH
 #define SUBDIVIDERPLUGIN_HH
 
-/** Simple Subdivider Plugin based on Subdivider code from Martin Habbeke
- */
 #include <QObject>
 #include <QMenuBar>
 
@@ -17,10 +58,6 @@
 #include <ObjectTypes/TriangleMesh/TriangleMesh.hh>
 
 #include "subdividerToolbar.hh"
-
-// Subdivider types
-//typename OpenMesh::Subdivider::Uniform:LoopT<TriMesh>  LoopT;
-//typename OpenMesh::Subdivider::Uniform:Sqrt3T<TriMesh> Sqrt3T;
 
 class SubdividerPlugin : public QObject, BaseInterface , ToolboxInterface, LoggingInterface, LoadSaveInterface, ScriptInterface, BackupInterface
 {
