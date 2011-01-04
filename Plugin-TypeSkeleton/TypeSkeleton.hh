dc4525c8a784dcb69ed80c03ce9faca5933c475c
wilden <wilden@383ad7c9-94d9-4d36-a494-682f7c89f535>
2011-01-04 13:57:02 +0000

    typeSkeleton plugin

---
+++ Plugin-TypeSkeleton/TypeSkeleton.hh
@@ -0,0 +1,121 @@
+/*===========================================================================*\
+ *                                                                           *
+ *                              OpenFlipper                                  *
+ *      Copyright (C) 2001-2009 by Computer Graphics Group, RWTH Aachen      *
+ *                           www.openflipper.org                             *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *  This file is part of OpenFlipper.                                        *
+ *                                                                           *
+ *  OpenFlipper is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
+ *                                                                           *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
+ *                                                                           *
+ *  OpenFlipper is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with OpenFlipper. If not,                                  *
+ *  see <http://www.gnu.org/licenses/>.                                      *
+ *                                                                           *
+\*===========================================================================*/
+
+/*===========================================================================*\
+ *                                                                           *
+ *   $Revision: 7683 $                                                       *
+ *   $Author: wilden $                                                       *
+ *   $Date: 2010-04-12 13:51:21 +0100 (Mo, 12. Apr 2010) $                   *
+ *                                                                           *
+\*===========================================================================*/
+
+
+
+
+#ifndef TYPESKELETONPLUGIN_HH
+#define TYPESKELETONPLUGIN_HH
+
+#include <QObject>
+
+#include <OpenFlipper/common/Types.hh>
+#include <ObjectTypes/Skeleton/Skeleton.hh>
+#include <OpenFlipper/BasePlugin/BaseInterface.hh>
+#include <OpenFlipper/BasePlugin/LoadSaveInterface.hh>
+#include <OpenFlipper/BasePlugin/LoggingInterface.hh>
+#include <OpenFlipper/BasePlugin/TypeInterface.hh>
+#include <OpenFlipper/BasePlugin/ContextMenuInterface.hh>
+
+class TypeSkeletonPlugin : public QObject, BaseInterface, LoadSaveInterface, LoggingInterface, TypeInterface, ContextMenuInterface
+{
+   Q_OBJECT
+   Q_INTERFACES(BaseInterface)
+   Q_INTERFACES(LoadSaveInterface)
+   Q_INTERFACES(LoggingInterface)
+   Q_INTERFACES(TypeInterface)
+   Q_INTERFACES(ContextMenuInterface)
+
+  signals:
+    // Logging interface
+    void log(Logtype _type, QString _message);
+    void log(QString _message);
+    
+    // LoadSave Interface
+    void emptyObjectAdded( int _id );
+
+    // ContextMenuInterface
+    void addContextMenuItem(QAction* _action , ContextMenuType _type);
+    void addContextMenuItem(QAction* _action , DataType _objectType , ContextMenuType _type );
+    
+  private slots:
+    void pluginsInitialized();
+    void noguiSupported( ) {} ;
+    
+    void slotUpdateContextMenu( int _objectId );
+    
+    void slotShowIndices();
+    void slotShowCoordFrames();
+    void slotShowMotionPath();
+
+  private:
+    QAction* showIndicesAction_;
+    QAction* showCoordFramesAction_;
+    QAction* showMotionAction_;
+    
+  public :
+
+     ~TypeSkeletonPlugin() {};
+     TypeSkeletonPlugin();
+
+     QString name() { return (QString("TypeSkeleton")); };
+     QString description( ) { return (QString(tr("Register Skeleton type"))); };
+     
+     bool registerType();
+
+  public slots:
+
+    // Base Interface
+    QString version() { return QString("1.0"); };
+    
+    // Type Interface
+    int addEmpty();
+    DataType supportedType() { return DATA_SKELETON; }; 
+    
+  private:
+    
+    /// Return unique name for object
+    QString get_unique_name(SkeletonObject* _object);
+
+};
+
+#endif //TYPESKELETONPLUGIN_HH
