f3fa6715adb622babe8bda52a2edbb55a0922ba1
wilden <wilden@383ad7c9-94d9-4d36-a494-682f7c89f535>
2011-01-04 13:57:02 +0000

    typeSkeleton plugin

---
+++ branches/Free/Plugin-TypeSkeleton/TypeSkeleton.cc
@@ -0,0 +1,236 @@
+/*===========================================================================*\
+ *                                                                           *
+ *                              OpenFlipper                                  *
+ *      Copyright (C) 2001-2009 by Computer Graphics Group, RWTH Aachen      *
+ *                           www.openflipper.org                             *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *  This file is part of OpenFlipper.                                        *
+ *                                                                           *
+ *  OpenFlipper is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
+ *                                                                           *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
+ *                                                                           *
+ *  OpenFlipper is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with OpenFlipper. If not,                                  *
+ *  see <http://www.gnu.org/licenses/>.                                      *
+ *                                                                           *
+\*===========================================================================*/
+
+/*===========================================================================*\
+ *                                                                           *
+ *   $Revision: 7683 $                                                       *
+ *   $Author: wilden $                                                       *
+ *   $Date: 2010-04-12 13:51:21 +0100 (Mo, 12. Apr 2010) $                   *
+ *                                                                           *
+\*===========================================================================*/
+
+
+#include "TypeSkeleton.hh"
+
+#include "OpenFlipper/BasePlugin/PluginFunctions.hh"
+
+TypeSkeletonPlugin::TypeSkeletonPlugin()
+  : showIndicesAction_(0),
+    showCoordFramesAction_(0),
+    showMotionAction_(0)
+{
+}
+
+/** \brief Second initialization phase
+ *
+ */
+void TypeSkeletonPlugin::pluginsInitialized()
+{
+  
+  if ( OpenFlipper::Options::gui() ){
+  
+    QMenu* contextMenu = new QMenu("Options");
+    
+    QString iconPath = OpenFlipper::Options::iconDirStr()+OpenFlipper::Options::dirSeparator();
+
+    //Show indices
+    showIndicesAction_ = new QAction(tr("Show &Indices"), this);
+    showIndicesAction_->setStatusTip(tr("Show Joint Indices"));
+    showIndicesAction_->setIcon( QIcon(iconPath + "showIndices.png") );
+    showIndicesAction_->setCheckable(true);
+    showIndicesAction_->setChecked(false);
+    //Show coord-frames
+    showCoordFramesAction_ = new QAction(tr("Show &Coordinate Frames"), this);
+    showCoordFramesAction_->setStatusTip(tr("Show Coordinate Frames for joints"));
+    showCoordFramesAction_->setIcon( QIcon(iconPath + "coordsys.png") );
+    showCoordFramesAction_->setCheckable(true);
+    showCoordFramesAction_->setChecked(false);
+    //Show motion space
+    showMotionAction_ = new QAction(tr("Show &Motion Path"), this);
+    showMotionAction_->setStatusTip(tr("Show path of motions for joints"));
+    showMotionAction_->setIcon( QIcon(iconPath + "motionPath.png") );
+    showMotionAction_->setCheckable(true);
+    showMotionAction_->setChecked(false);
+    
+    connect(showIndicesAction_,     SIGNAL(triggered()), this, SLOT(slotShowIndices()) );
+    connect(showCoordFramesAction_, SIGNAL(triggered()), this, SLOT(slotShowCoordFrames()) );
+    connect(showMotionAction_,      SIGNAL(triggered()), this, SLOT(slotShowMotionPath()) );
+    
+    contextMenu->addAction(showIndicesAction_);
+    contextMenu->addAction(showCoordFramesAction_);
+    contextMenu->addAction(showMotionAction_);
+
+    emit addContextMenuItem(contextMenu->menuAction(), DATA_SKELETON, CONTEXTOBJECTMENU);
+  }
+}
+
+void TypeSkeletonPlugin::slotUpdateContextMenu( int _objectId )
+{
+  if ( _objectId == -1)
+    return;
+
+  BaseObjectData* object;
+  if ( !PluginFunctions::getObject(_objectId,object) )
+    return;
+
+  SkeletonObject* skeletonObject = dynamic_cast<SkeletonObject*>(object);
+  
+  if(skeletonObject != 0){
+    showIndicesAction_->setChecked( skeletonObject->indicesVisible() );
+    showCoordFramesAction_->setChecked( skeletonObject->skeletonNode()->coordFramesVisible() );
+    showMotionAction_->setChecked( skeletonObject->motionPathVisible() );
+  }
+}
+
+void TypeSkeletonPlugin::slotShowIndices(){
+  
+  QVariant contextObject = showIndicesAction_->data();
+  int objectId = contextObject.toInt();
+
+  if ( objectId == -1)
+    return;
+
+  BaseObjectData* object;
+  if ( !PluginFunctions::getObject(objectId,object) )
+    return;
+
+  SkeletonObject* skeletonObject = dynamic_cast<SkeletonObject*>(object);
+  
+  if(skeletonObject != 0){
+    skeletonObject->showIndices( showIndicesAction_->isChecked() );
+    emit updatedObject( objectId, UPDATE_ALL );
+  }
+}
+
+void TypeSkeletonPlugin::slotShowCoordFrames(){
+  
+  QVariant contextObject = showCoordFramesAction_->data();
+  int objectId = contextObject.toInt();
+
+  if ( objectId == -1)
+    return;
+
+  BaseObjectData* object;
+  if ( !PluginFunctions::getObject(objectId,object) )
+    return;
+
+  SkeletonObject* skeletonObject = dynamic_cast<SkeletonObject*>(object);
+  
+  if(skeletonObject != 0){
+    skeletonObject->skeletonNode()->showCoordFrames( showCoordFramesAction_->isChecked() );
+    emit updatedObject( objectId, UPDATE_ALL );
+  }
+}
+
+void TypeSkeletonPlugin::slotShowMotionPath(){
+  
+  QVariant contextObject = showMotionAction_->data();
+  int objectId = contextObject.toInt();
+
+  if ( objectId == -1)
+    return;
+
+  BaseObjectData* object;
+  if ( !PluginFunctions::getObject(objectId,object) )
+    return;
+
+  SkeletonObject* skeletonObject = dynamic_cast<SkeletonObject*>(object);
+  
+  if(skeletonObject != 0){
+    skeletonObject->showMotionPath( showMotionAction_->isChecked() );
+    emit updatedObject( objectId, UPDATE_ALL );
+  }
+}
+
+bool TypeSkeletonPlugin::registerType() {
+  addDataType("Skeleton",tr("Skeleton"));
+  setTypeIcon("Skeleton", "SkeletonType.png");
+  return true;
+}
+
+int TypeSkeletonPlugin::addEmpty(){
+    
+  // new object data struct
+  SkeletonObject * object = new SkeletonObject();
+
+  if ( PluginFunctions::objectCount() == 1 )
+    object->target(true);
+
+  if (PluginFunctions::targetCount() == 0 )
+    object->target(true);
+
+  QString name = get_unique_name(object);
+
+  // call the local function to update names
+  QFileInfo f(name);
+  object->setName( f.fileName() );
+
+  object->update();
+
+  object->show();
+
+  emit emptyObjectAdded (object->id() );
+
+  return object->id();
+}
+
+QString TypeSkeletonPlugin::get_unique_name(SkeletonObject* _object)
+{
+  bool name_unique = false;
+
+  int cur_idx = _object->id();
+
+  while(!name_unique)
+  {
+    name_unique = true;
+
+    QString cur_name = QString(tr("Skeleton %1").arg( cur_idx ));
+
+    PluginFunctions::ObjectIterator o_it(PluginFunctions::ALL_OBJECTS, DATA_SKELETON );
+    for(; o_it != PluginFunctions::objectsEnd(); ++o_it)
+    {
+      if( o_it->name() == cur_name)
+      {
+        name_unique = false;
+        cur_idx += 10;
+        break;
+      }
+    }
+  }
+
+  return QString(tr("Skeleton %1").arg( cur_idx ));
+}
+
+Q_EXPORT_PLUGIN2( typeskeletonplugin , TypeSkeletonPlugin );
+
