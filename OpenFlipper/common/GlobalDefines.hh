4e51120121ffe7cd537895f8ec09f869a68f1c98
moebius <moebius@informatik.rwth-aachen.de>
2011-01-13 16:39:36 +0000

    Remove qt 4.7 warning

--- OpenFlipper/common/GlobalDefines.hh
+++ OpenFlipper/common/GlobalDefines.hh
@@ -67,12 +67,12 @@
 #define GCC_VERSION (__GNUC__ * 1000 + __GNUC_MINOR__ * 10)
 
 
-#if QT_VERSION >= 0x040700
+#if QT_VERSION > 0x040700
   #ifdef WIN32
-    #pragma message("Detected QT Version 4.7 or higher!")
+    #pragma message("Detected QT Version 4.8 or higher!")
     #pragma message("OpenFlipper has not been tested with this QT Version.")
   #else
-    #warning Detected QT Version 4.7 or higher!
+    #warning Detected QT Version 4.8 or higher!
     #warning OpenFlipper has not been tested with this QT Version.
   #endif
 #endif
