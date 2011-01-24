7447a3b572faeb4c17456bd03f64c07b75d54fa7
frickenschmidt <frickenschmidt@383ad7c9-94d9-4d36-a494-682f7c89f535>
2011-01-24 14:44:46 +0000

    Fixed bug that rotating an object didn't update the normals, leading to wrong lighting.

--- Plugin-Move/MovePlugin.cc
+++ Plugin-Move/MovePlugin.cc
@@ -439,11 +439,14 @@ void MovePlugin::moveObject(ACG::Matrix4x4d mat, int _id) {
 
   if  ( object->dataType()  == DATA_TRIANGLE_MESH ) {
     transformMesh(mat , *PluginFunctions::triMesh(object) );
+    PluginFunctions::triMesh(object)->update_normals();
   } else  if  ( object->dataType()  == DATA_POLY_MESH ) {
     transformMesh(mat , *PluginFunctions::polyMesh(object) );
+    PluginFunctions::polyMesh(object)->update_normals();
 #ifdef ENABLE_TSPLINEMESH_SUPPORT
   } else  if  ( object->dataType()  == DATA_TSPLINE_MESH ) {
     transformMesh(mat , *PluginFunctions::tsplineMesh(object) );
+    PluginFunctions::tsplineMesh(object)->update_normals();
 #endif
 #ifdef ENABLE_POLYLINE_SUPPORT
   } else  if  ( object->dataType()  == DATA_POLY_LINE ) {
