8b93a3e014370d8eeeac565023b06e1f757e1710
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2011-03-01 17:20:44 +0000

    Mouse Interface Documentation

--- branches/Free/OpenFlipper/BasePlugin/MouseInterface.hh
+++ branches/Free/OpenFlipper/BasePlugin/MouseInterface.hh
@@ -44,23 +44,22 @@
 #ifndef MOUSEINTERFACE_HH
 #define MOUSEINTERFACE_HH
 
- #include <QtGui>
+#include <QtGui>
 
-/** \brief Interface for all plugins which have to react on mouse events
-  *
-  * When using mouse events in your plugins implement this interface. Remember that
-  * all plugins may get these signals. Therefore you hav to check if the current pickingMode
-  * you want to react on is active before doing anything. Only the slotMouseEventIdentify is
-  * independent of picking modes.
-  *
-  * The slotMouseWheelEvent and slotMouseEvent only get called when the system is in picking
-  * mode.
-  *
-  * slotMouseEventIdentify will be used when question mode is active.
-  *
-  * See our tutorial \ref ex3 for an example of how to use mouse and keyboard events
-  * within a plugin.
-  */
+
+/** \file MouseInterface.hh
+*
+* Interface for registering types in OpenFlipper. \ref mouseInterfacePage
+*/
+
+
+ /** \brief Interface class for receiving mouse events
+ *
+ * \n
+ * \ref mouseInterfacePage "Detailed description"
+ * \n
+ *
+ */
 class MouseInterface {
 
   private slots :
@@ -115,6 +114,72 @@ class MouseInterface {
     virtual ~MouseInterface() {};
 
 };
+
+/** \page mouseInterfacePage Mouse Interface
+\n
+\image html mouseInterface.png
+\n
+
+
+The mouse interface can be used to receive mouse events which occur in the glViewer.
+
+There are 4 main viewer modes:
+<ul>
+<li> <b>Move Mode</b> events are never passed to one of the plugins and is only used for scene navigation
+inside the viewers.
+<li> <b>Picking Mode</b> events where passed through the MouseInterface::slotMouseEvent() and MouseInterface::slotMouseWheelEvent().
+<li> <b>Light Mode</b> events where passed through the MouseInterface::slotMouseEventLight()
+<li> <b>Identify Mode</b> events where passed through the MouseInterface::slotMouseEventIdentify()
+</ul>
+
+You can add handlers for these mouse events in your plugin. Remember that all plugins receive these signals.\n
+
+The picking mode is only a global mode which is devided into several other picking modes that can be managed
+through the PickingInterface. If you react on mouse events from picking mode, you should check if the current
+picking mode is yours.
+\code
+void MousePlugin::slotMouseEvent(QMouseEvent* _event) {
+
+  // Check if your pick mode is currently active
+  if ( PluginFunctions::pickMode() == "YourPickMode" && PluginFunctions::actionMode() == Viewer::PickingMode ) {
+
+    // If double click has been performed
+    if (_event->type() == QEvent::MouseButtonDblClick) {
+      unsigned int node_idx, target_idx;
+      OpenMesh::Vec3d hitPoint;
+
+      // Get picked object's identifier by picking in scenegraph
+      if ( PluginFunctions::scenegraphPick(ACG::SceneGraph::PICK_ANYTHING,_event->pos(), node_idx, target_idx, &hitPoint) ) {
+
+        BaseObjectData* object;
+
+        // Get picked object
+        if ( PluginFunctions::getPickedObject(node_idx, object) ) {
+
+          // Do something with the object
+
+        }
+      }
+    }
+  }
+
+
+}
+\endcode
+
+See our tutorial \ref ex3 for an example of how to use mouse and keyboard events
+within a plugin.
+
+To use the MouseInterface:
+<ul>
+<li> include MouseInterface.hh in your plugins header file
+<li> derive your plugin from the class MouseInterface
+<li> add Q_INTERFACES(MouseInterface) to your plugin class
+<li> And add the signals or slots you want to use to your plugin class (You don't need to implement all of them)
+</ul>
+
+
+*/
 
 Q_DECLARE_INTERFACE(MouseInterface,"OpenFlipper.MouseInterface/1.0")
 
