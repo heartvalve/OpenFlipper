11a1439808f3c352cf510f9af0d1eb37cc6de32d
moebius <moebius@informatik.rwth-aachen.de>
2011-03-15 15:18:43 +0000

    KeyInterface Documentation

--- OpenFlipper/BasePlugin/KeyInterface.hh
+++ OpenFlipper/BasePlugin/KeyInterface.hh
@@ -46,16 +46,20 @@
 
 #include <QtGui>
  
- /** \brief Interface class for plugins which have to react on keyboard  events
-  * 
-  * Using this interface you can react on key events. You first have to register
-  * a key (or key combination). For each registered key you will receive a keyEvent when
-  * the key was pressed and a keyReleaseEvent when the key was released.
-  *
-  * See our tutorial \ref ex3 for an example of how to use mouse and keyboard events
-  * within a plugin.
-  *
-  */ 
+
+
+/** \file KeyInterface.hh
+*
+* Interface for receiving keyboard events inside your plugin.\ref keyInterfacePage
+*/
+
+/** \brief Keyboard Event Interface
+ *
+ * \ref keyInterfacePage "Detailed description"
+ * \n
+ *
+ * Using this interface you can react on key events.
+ */
 class KeyInterface {
    signals:
       /**  \brief Register a key-combination for your plugin
@@ -68,7 +72,7 @@ class KeyInterface {
        * @param _description a short description about the functionality
        * @param _multiUse can the key additionally be registered by another plugin
        */
-      virtual void registerKey(int /*_key*/, Qt::KeyboardModifiers /*_modifiers*/, QString /*_description*/, bool /*_multiUse*/ = false) {};
+      virtual void registerKey(int _key, Qt::KeyboardModifiers _modifiers, QString _description, bool _multiUse = false) {};
 
    public slots :
       
@@ -77,14 +81,14 @@ class KeyInterface {
        * This slot is called if a key event occured in the Viewer 
        * @param _event Keyevent
       */
-      virtual void slotKeyEvent( QKeyEvent* /*_event*/ ) {};
+      virtual void slotKeyEvent( QKeyEvent* _event ) {};
       
       /**  \brief Key Release Event from Main App
        * 
        * This slot is called if a key release event occured in the Viewer 
        * @param _event Keyevent
       */
-      virtual void slotKeyReleaseEvent( QKeyEvent* /*_event*/ ) {};
+      virtual void slotKeyReleaseEvent( QKeyEvent* _event ) {};
       
    public : 
        
@@ -92,6 +96,78 @@ class KeyInterface {
       virtual ~KeyInterface() {};
       
 };
+
+
+/** \page keyInterfacePage Key Interface
+\image html keyInterface.png
+\n
+
+Using this interface you can react on key events. You first have to register
+a key (or key combination) with KeyInterface::registerKey().
+Note that the log widget will contain a warning if you register
+a key twice or it is occupied by another plugin.
+
+For each registered key your KeyInterface::slotKeyEvent() will be triggered when
+the key was pressed and the slot KeyInterface::slotKeyReleaseEvent() when the key
+was released. Both slots get the QKeyEvent and will contain all the modifiers
+that were active when the event occurred.
+
+In OpenFlippers options tab you can view all registered keys and reassign them.
+The events will be mapped, such that the plugins still receive the right combination.
+Therefore the mapping will be completely invisible to them.
+
+See our tutorial \ref ex3 for an example of how to use mouse and keyboard events
+within a plugin.
+
+To use the KeyInterface:
+<ul>
+<li> include KeyInterface.hh in your plugins header file
+<li> derive your plugin from the class KeyInterface
+<li> add Q_INTERFACES(KeyInterface) to your plugin class
+<li> Implement the required functions.
+</ul>
+
+\code
+
+// In your plugin initialization register the keys you want to get.
+void ExamplePlugin::initializePlugin() {
+
+  // Register keys
+  emit registerKey(Qt::Key_W,     Qt::NoModifier, "Rotate object down");
+  emit registerKey(Qt::Key_A,     Qt::NoModifier, "Rotate object left");
+  emit registerKey(Qt::Key_D,     Qt::NoModifier, "Rotate object right");
+  emit registerKey(Qt::Key_S,     Qt::NoModifier, "Rotate object up");
+
+}
+
+// Called when one of the registered Keys is pressed.
+void MouseAndKeyPlugin::slotKeyEvent( QKeyEvent* _event ) {
+
+  // Check if it's really the right key
+  if ( _event->modifiers() == Qt::NoModifier ) {
+
+    // Switch pressed keys
+    switch (_event->key()) {
+      case Qt::Key_W:
+        // Move forward
+        break;
+      case Qt::Key_A:
+        // Move left
+        break;
+      case Qt::Key_D:
+        // Move right
+        break;
+      case Qt::Key_S:
+        // Move backward
+        break;
+      default:
+        break;
+    }
+
+  }
+}
+\endcode
+*/
 
 Q_DECLARE_INTERFACE(KeyInterface,"OpenFlipper.KeyInterface/1.1")
       
