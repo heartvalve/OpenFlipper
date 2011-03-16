ebfcd7eabda7847091f3e762c8b535d672c5e83c
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2011-03-16 11:12:57 +0000

    Load Save Interface Documentation

--- branches/Free/OpenFlipper/BasePlugin/LoadSaveInterface.hh
+++ branches/Free/OpenFlipper/BasePlugin/LoadSaveInterface.hh
@@ -71,7 +71,7 @@ class LoadSaveInterface {
        * @param _id Object to Save
        * @param _filename Filename to save it to (Leave as "" to automatically determine filename)
       */
-      virtual void save(int /*_id*/ , QString /*_filename*/ ) {}
+      virtual void save(int _id , QString _filename ) {}
 
       /** \brief Load object from file with a specific DataType
        *
@@ -79,7 +79,7 @@ class LoadSaveInterface {
        * @param _type Type to be loaded
        * @param _id Here the id of the loaded object is returned. id its -1 something went wrong
        */
-      virtual void load(QString /*_filename*/, DataType /*_type*/, int& /*_id*/) {};
+      virtual void load(QString _filename, DataType _type, int& _id) {};
 
 
       /** Add an empty object of the given type
@@ -87,26 +87,29 @@ class LoadSaveInterface {
        * @param _type Type to be created
        * @param _id Here the id of the loaded object is returned. id its -1 something went wrong
        */
-      virtual void addEmptyObject( DataType /*_type*/, int& /*_id*/) {};
+      virtual void addEmptyObject( DataType _type, int& _id) {};
 
       /** Create a copy of an existing object
        *
        * @param _oldId id of the object to copy
        * @param _newId id of the new object created
        */
-      virtual void copyObject( int /*_oldId*/, int& /*_newId*/) {};
+      virtual void copyObject( int _oldId, int& _newId) {};
 
 
-      /** \brief Emit this signal if an empty object has been created
+      /** \brief DEPRECATED! Emit this signal if an empty object has been created
+       *
+       * Deprecated! Objects are added by the Type Plugins and this is signal is in the TypeInterface now
+       *
        * @param _id Id of the added object
        */
-      virtual void emptyObjectAdded( int /*_id*/ ) {};
+      virtual void emptyObjectAdded( int _id ) {};
 
       /** \brief Delete an object
        *
        * @param _id Id of the object
        */
-      virtual void deleteObject( int /*_id*/ ){};
+      virtual void deleteObject( int _id ){};
 
       /** \brief Delete all Objects
        *
@@ -120,18 +123,18 @@ class LoadSaveInterface {
      *  This slot is called if a file has been opened by the core.\n
      *  @param _id Id of the new object
      */
-    virtual void fileOpened( int /*_id*/ ) {};
+    virtual void fileOpened( int _id ) {};
 
     /**  \brief An empty object has been added
      *
      *  This slot is called if an empty object has been added by the core.\n
      *  @param _id Id of the new object
      */
-    virtual void addedEmptyObject( int /*_id*/ ) {};
+    virtual void addedEmptyObject( int _id ) {};
 
     /** \brief An object was deleted
       *
-      * This function is called bz the core if an object gets deleted. It is called immediatly
+      * This function is called by the core if an object gets deleted. It is called immediately
       * before the object is removed from the scenegraph. So if this function is invoked, the object still
       * exists. All plugins get informed via this slot.\
       *
@@ -140,7 +143,7 @@ class LoadSaveInterface {
       *
       * @param _id Id of the object that is deleted.
       */
-    virtual void objectDeleted( int /*_id*/ ){};
+    virtual void objectDeleted( int _id ){};
 
 };
 
@@ -149,9 +152,53 @@ class LoadSaveInterface {
 The LoadSaveInterface can be used by plugins to add new objects to the scene either by creating empty objects
 or by loading them from files. The interface also triggers saving of existing objects to files.
 
+
+\section loadSaveInterface_load_save Load/Save Files
+You can load a file from within your plugin via
+\code
+  emit LoadSaveInterface::load(QString _filename, DataType _type, int& _id);
+\endcode
+and to save an object to a file, call LoadSaveInterface::
+\code
+  emit LoadSaveInterface::save(int _id , QString _filename );
+\endcode
+
+The corresponding File Plugin will than be activated and load or save the object. for load, the id of the new object
+is returned.
+
+\section loadSaveInterface_createObjects Creating/Copying Objects
+To add objects to the scene you can use the following two functions:
+\code
+  emit LoadSaveInterface::addEmptyObject( DataType _type, int& _id);
+\endcode
+This will create a new object of the given DataType and return the id of it, while
+\code
+  emit LoadSaveInterface::copyObject( int _oldId, int& _newId);
+\endcode
+will copy the given object and return the new object id.
+
+\section loadSaveInterface_deleteObjects Deleting Objects
+
+You can delete a specific object via
+\code
+  emit LoadSaveInterface::deleteObject( int _id );
+\endcode
+or clear the whole scene with:
+\code
+  emit LoadSaveInterface::deleteAllObjects();
+\endcode
+
+\section loadSaveInterface_callbacks Object Information
 Additionally the interface informs plugins if new objects have been added to the scenes or if previous objects
 have been removed.
+LoadSaveInterface::fileOpened( int _id ) will be triggered each time an object is loaded from a file. If simply an
+empty object has been added, the slot LoadSaveInterface::addedEmptyObject( int _id ) will be called. If an object
+gets deleted, the LoadSaveInterface::objectDeleted( int _id ) will be called immediately before the object is removed
+from the scenegraph. So if this function is invoked, the object still exists. All plugins get informed via this slot.
 
+If you want to know if the whole scene got cleared, use BaseInterface::slotAllCleared( ).
+
+\section Usage
 To use the LoadSaveInterface:
 <ul>
 <li> include LoadSaveInterface in your plugins header file
@@ -159,9 +206,6 @@ To use the LoadSaveInterface:
 <li> add Q_INTERFACES(LoadSaveInterface) to your plugin class
 <li> And add the signals or slots you want to use to your plugin class (You don't need to implement all of them)
 </ul>
-
-
-
 
 */
 
