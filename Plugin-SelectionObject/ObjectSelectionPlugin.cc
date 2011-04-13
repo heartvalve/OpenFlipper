08b72747be411b0e98e240f818f00941d0440fbd
kremer <mkremer@informatik.rwth-aachen.de>
2011-04-13 08:02:09 +0000

    Forgot return... :)

--- Plugin-SelectionObject/ObjectSelectionPlugin.cc
+++ Plugin-SelectionObject/ObjectSelectionPlugin.cc
@@ -163,9 +163,10 @@ void ObjectSelectionPlugin::slotToggleSelection(QMouseEvent* _event, SelectionIn
     if((_currentType & objectType_) == 0) return;
 
     // Return if mouse event is not a left-button click
-    if(_event->button() == Qt::LeftButton)
+    if(_event->button() == Qt::LeftButton) return;
      
-    unsigned int node_idx, target_idx;
+    unsigned int node_idx = 0;
+    unsigned int target_idx = 0;
     ACG::Vec3d hit_point;
     
     BaseObjectData* object = 0;
