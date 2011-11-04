94f4118a593ee46306afc41a1dc0b010e0947383
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2011-11-04 07:55:42 +0000

    Renamed to Remesher

---
+++ branches/Free/Plugin-Remesher/Algorithms/DiffGeoT.cc
@@ -0,0 +1,476 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2011 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision$                                                       *
+*   $LastChangedBy$                                                *
+*   $Date$                    *
+*                                                                            *
+\*===========================================================================*/
+
+//=============================================================================
+//
+//  CLASS DiffGeoT - IMPLEMENTATION
+//
+//=============================================================================
+
+#define DIFFGEOT_C
+
+//== INCLUDES =================================================================
+
+//#include <mb_base/mb_base.hh>
+#include "DiffGeoT.hh"
+
+#ifdef WIN32
+#undef min
+#undef max
+#endif
+
+
+//== NAMESPACES ===============================================================
+
+namespace Remeshing {
+
+//== IMPLEMENTATION ==========================================================
+
+
+template <class Mesh>
+DiffGeoT<Mesh>::
+DiffGeoT(Mesh& _mesh) 
+  : mesh_(_mesh),
+    weights_computed_(false),
+    area_computed_(false)
+{
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+DiffGeoT<Mesh>::
+~DiffGeoT()
+{
+  mesh_.remove_property(area_);
+  mesh_.remove_property(gauss_curvature_);
+  mesh_.remove_property(mean_curvature_);
+  mesh_.remove_property(edge_weight_);
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+void 
+DiffGeoT<Mesh>::
+compute(unsigned int _post_smoothing_iters)
+{
+  compute_edge_weights();
+  compute_area();
+  compute_gauss_curvature();
+  compute_mean_curvature();
+  post_smoothing(_post_smoothing_iters);
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+void 
+DiffGeoT<Mesh>::
+compute_edge_weights()
+{
+  if (!edge_weight_.is_valid())
+    mesh_.add_property(edge_weight_);
+
+
+  typename Mesh::EdgeIter          e_it, e_end(mesh_.edges_end());
+  typename Mesh::HalfedgeHandle    heh0, heh1, heh2;
+  const typename Mesh::Point       *p0, *p1, *p2;
+  typename Mesh::Point             d0, d1;
+  typename Mesh::Scalar            weight;
+
+
+  for (e_it=mesh_.edges_begin(); e_it!=e_end; ++e_it)
+  {
+    heh0   = mesh_.halfedge_handle(e_it, 0);
+    p0     = &mesh_.point(mesh_.to_vertex_handle(heh0));
+
+    heh1   = mesh_.halfedge_handle(e_it, 1);
+    p1     = &mesh_.point(mesh_.to_vertex_handle(heh1));
+
+    heh2   = mesh_.next_halfedge_handle(heh0);
+    p2     = &mesh_.point(mesh_.to_vertex_handle(heh2));
+    d0     = (*p0 - *p2).normalize();
+    d1     = (*p1 - *p2).normalize();
+    weight = 1.0 / tan(acos(d0|d1));
+
+    heh2   = mesh_.next_halfedge_handle(heh1);
+    p2     = &mesh_.point(mesh_.to_vertex_handle(heh2));
+    d0     = (*p0 - *p2).normalize();
+    d1     = (*p1 - *p2).normalize();
+    weight += 1.0 / tan(acos(d0|d1));
+
+    mesh_.property(edge_weight_, e_it) = weight;
+  }
+
+
+  weights_computed_ = true;
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+void 
+DiffGeoT<Mesh>::
+compute_area()
+{
+  if (!area_.is_valid())
+    mesh_.add_property(area_);
+
+
+  typename Mesh::VertexIter  v_it, v_end(mesh_.vertices_end());
+
+
+  for (v_it=mesh_.vertices_begin(); v_it!=v_end; ++v_it)
+    mesh_.property(area_, v_it) = compute_area(v_it);
+
+
+  area_computed_ = true;
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+typename Mesh::Scalar
+DiffGeoT<Mesh>::
+compute_area(VertexHandle _vh) const
+{
+  typename Mesh::HalfedgeHandle    heh0, heh1, heh2;
+  typename Mesh::VertexVertexIter  vv_it;
+
+  ACG::Vec3d   P, Q, R, PQ, QR, PR;
+  double  normPQ, normQR, normPR;
+  double  angleP, angleQ, angleR;
+  double  area;
+  double  ub(0.999), lb(-0.999);
+  const double epsilon( 1e-12 );
+
+  area = 0.0;
+
+  for (vv_it=mesh_.vv_iter(_vh); vv_it; ++vv_it)
+  {
+    heh0 = vv_it.current_halfedge_handle();
+    heh1 = mesh_.next_halfedge_handle(heh0);
+    heh2 = mesh_.next_halfedge_handle(heh1);
+
+    if (mesh_.is_boundary(heh0)) continue;
+
+    ACG::Vec3d P = (ACG::Vec3d) mesh_.point(mesh_.to_vertex_handle(heh2));
+    ACG::Vec3d Q = (ACG::Vec3d) mesh_.point(mesh_.to_vertex_handle(heh0));
+    ACG::Vec3d R = (ACG::Vec3d) mesh_.point(mesh_.to_vertex_handle(heh1));
+
+    (PQ = Q) -= P;
+    (QR = R) -= Q;
+    (PR = R) -= P;
+      
+    normPQ = PQ.norm();
+    normQR = QR.norm();
+    normPR = PR.norm();
+
+    if( normPQ <= epsilon || normQR <= epsilon || normPR <= epsilon )
+	continue;
+
+    angleP =  (PQ | PR) / normPQ / normPR;
+    angleQ = -(PQ | QR) / normPQ / normQR;
+    angleR =  (QR | PR) / normQR / normPR;
+
+    if (angleP > ub) angleP = ub;  else if (angleP < lb) angleP = lb;
+    if (angleQ > ub) angleQ = ub;  else if (angleQ < lb) angleQ = lb;
+    if (angleR > ub) angleR = ub;  else if (angleR < lb) angleR = lb;
+
+    angleP = acos(angleP);
+    angleQ = acos(angleQ);
+    angleR = acos(angleR);
+
+    if (angleP >= M_PI_2 || angleQ >= M_PI_2 || angleR >= M_PI_2)
+    {
+      if (angleP >= M_PI_2)
+	area += 0.25 * (PQ % PR).norm();
+      else
+	area += 0.125 * (PQ % PR).norm();
+    }
+    else
+    {
+      area += 0.125 * (normPR * normPR / tan(angleQ) + 
+		       normPQ * normPQ / tan(angleR));
+    }
+  }
+    
+  return area;
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+void 
+DiffGeoT<Mesh>::
+compute_gauss_curvature()
+{
+  if (!gauss_curvature_.is_valid())
+    mesh_.add_property(gauss_curvature_);
+
+  if (!area_computed_)
+    compute_area();
+
+
+  typename Mesh::VertexIter        v_it, v_end(mesh_.vertices_end());
+  typename Mesh::VertexVertexIter  vv_it, vv_it2;
+  typename Mesh::Point             d0, d1;
+  typename Mesh::Scalar            curv, count;
+  typename Mesh::Scalar            angles, cos_angle;
+  typename Mesh::Scalar            lb(-1.0), ub(1.0);
+
+
+  // compute for all non-boundary vertices
+  for (v_it=mesh_.vertices_begin(); v_it!=v_end; ++v_it)
+  {
+    if (!mesh_.is_boundary(v_it))
+    {
+      angles = 0.0;
+
+      for (vv_it=mesh_.vv_iter(v_it); vv_it; ++vv_it)
+      {
+	vv_it2 = vv_it; ++vv_it2;
+
+	d0 = (mesh_.point(vv_it)  - mesh_.point(v_it)).normalize();
+	d1 = (mesh_.point(vv_it2) - mesh_.point(v_it)).normalize();
+      
+	cos_angle = std::max(lb, std::min(ub, (d0 | d1)));
+	angles += acos(cos_angle);
+      }
+
+      mesh_.property(gauss_curvature_, v_it) = 
+	(2*M_PI-angles) / mesh_.property(area_, v_it);
+    }
+  }
+
+
+  // boundary vertices: get from neighbors
+  for (v_it=mesh_.vertices_begin(); v_it!=v_end; ++v_it)
+  {
+    if (mesh_.is_boundary(v_it))
+    {
+      curv = count = 0.0;
+
+      for (vv_it=mesh_.vv_iter(v_it); vv_it; ++vv_it)
+	if (!mesh_.is_boundary(vv_it))
+	{
+	  curv += mesh_.property(gauss_curvature_, vv_it);
+	  ++count;
+	}
+
+      if (count)
+	mesh_.property(gauss_curvature_, v_it) = curv / count;
+    }
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+void 
+DiffGeoT<Mesh>::
+compute_mean_curvature()
+{
+  if (!mean_curvature_.is_valid())
+    mesh_.add_property(mean_curvature_);
+
+  if (!weights_computed_)
+    compute_edge_weights();
+
+  if (!area_computed_)
+    compute_area();
+
+
+  typename Mesh::VertexIter        v_it, v_end(mesh_.vertices_end());
+  typename Mesh::VertexVertexIter  vv_it;
+  typename Mesh::Scalar            weight;
+  typename Mesh::Point             umbrella;
+  typename Mesh::EdgeHandle        eh;
+  typename Mesh::Scalar            curv, count;
+
+
+  // compute for all non-boundary vertices
+  for (v_it=mesh_.vertices_begin(); v_it!=v_end; ++v_it)
+  {
+    if (!mesh_.is_boundary(v_it))
+    {
+      umbrella[0] = umbrella[1] = umbrella[2] = 0.0;
+
+      for (vv_it=mesh_.vv_iter(v_it); vv_it; ++vv_it)
+      {
+	eh        = mesh_.edge_handle(vv_it.current_halfedge_handle());
+	weight    = mesh_.property(edge_weight_, eh);
+	umbrella += (mesh_.point(v_it) - mesh_.point(vv_it)) * weight;
+      }
+    
+      mesh_.property(mean_curvature_, v_it) =
+	umbrella.norm() / (4.0 * mesh_.property(area_, v_it));
+    }
+  }
+
+
+  // boundary vertices: get from neighbors
+  for (v_it=mesh_.vertices_begin(); v_it!=v_end; ++v_it)
+  {
+    if (mesh_.is_boundary(v_it))
+    {
+      curv = count = 0.0;
+
+      for (vv_it=mesh_.vv_iter(v_it); vv_it; ++vv_it)
+	if (!mesh_.is_boundary(vv_it))
+	{
+	  curv += mesh_.property(mean_curvature_, vv_it);
+	  ++count;
+	}
+
+      if (count)
+	mesh_.property(mean_curvature_, v_it) = curv / count;
+    }
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+
+
+template <class Mesh>
+void 
+DiffGeoT<Mesh>::
+post_smoothing(unsigned int _iters)
+{
+  // early out
+  if (!_iters) return;
+
+
+  // something should already be there...
+  if (! (weights_computed_ && 
+	 area_computed_ &&
+	 mean_curvature_.is_valid() &&
+	 gauss_curvature_.is_valid()) )
+  {
+    std::cerr << "DiffGeoT::post_smoothing: something is wrong!\n";
+    return;
+  }
+
+
+
+  typename Mesh::VertexIter        v_it, v_end(mesh_.vertices_end());
+  typename Mesh::VertexVertexIter  vv_it;
+  typename Mesh::Scalar            w, ww;
+  typename Mesh::Scalar            gc, mc;
+  typename Mesh::EdgeHandle        eh;
+
+
+
+  // add helper properties to store new values
+  OpenMesh::VPropHandleT<Scalar>   new_gauss, new_mean;
+  mesh_.add_property(new_gauss);
+  mesh_.add_property(new_mean);
+
+
+
+  for (unsigned int i=0; i<_iters; ++i)
+  {
+
+    // compute new value
+    for (v_it=mesh_.vertices_begin(); v_it!=v_end; ++v_it)
+    {
+      if (!mesh_.is_boundary(v_it))
+      {
+        gc = mc = ww = 0.0;
+
+        for (vv_it=mesh_.vv_iter(v_it); vv_it; ++vv_it)
+        {
+          eh   = mesh_.edge_handle(vv_it.current_halfedge_handle());
+          ww  += (w = mesh_.property(edge_weight_, eh));
+          mc  += w * mesh_.property(mean_curvature_,  vv_it);
+          gc  += w * mesh_.property(gauss_curvature_, vv_it);
+        }
+        
+        if (ww)
+        {
+          mesh_.property(new_mean,  v_it) = mc / ww;
+          mesh_.property(new_gauss, v_it) = gc / ww;
+        }
+      }
+    }
+
+
+    // replace old by new value
+    for (v_it=mesh_.vertices_begin(); v_it!=v_end; ++v_it)
+    {
+      if (!mesh_.is_boundary(v_it))
+      {
+	mesh_.property(mean_curvature_,  v_it) = 
+	  mesh_.property(new_mean, v_it);
+
+	mesh_.property(gauss_curvature_,  v_it) = 
+	  mesh_.property(new_gauss, v_it);
+      }
+    }
+  }
+
+
+  // remove helper properties
+  mesh_.remove_property(new_gauss);
+  mesh_.remove_property(new_mean);
+}
+
+
+//=============================================================================
+} // namespace Remeshing
+//=============================================================================
