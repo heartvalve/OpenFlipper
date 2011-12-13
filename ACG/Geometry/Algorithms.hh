ea550af68c7da92ec7079b44a5b1cc2ec0507f17
kremer <mkremer@informatik.rwth-aachen.de>
2011-12-13 17:31:49 +0000

    Changed signature of rotationOfTowVectors function to just define the scalar type since we have to assume R^3 as underlying vector space.

--- ACG/Geometry/Algorithms.hh
+++ ACG/Geometry/Algorithms.hh
@@ -123,12 +123,12 @@ circumRadius( const VectorT<Scalar,3>&  _v0,
  * @param _axis A reference to a vector in which the rotation axis is stored
  * @param _angle A reference to a scalar type in which the signed angle is stored
  */
-template<typename Vec>
+template<typename Scalar>
 bool
-rotationOfTwoVectors( const Vec&  _v0,
-                      const Vec&  _v1,
-                      Vec&  _axis,
-                      typename Vec::value_type& _angle );
+rotationOfTwoVectors( const VectorT<Scalar,3>&  _v0,
+                      const VectorT<Scalar,3>&  _v1,
+                      VectorT<Scalar,3>&  _axis,
+                      typename Scalar& _angle );
 
 /** \brief Get determinant of matrix [_v1, _v2, _v3]
  */
