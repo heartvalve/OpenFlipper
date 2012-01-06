617cbf1f93ab1bfd767f193ee3190eaabac5a3d0
moebius <moebius@informatik.rwth-aachen.de>
2012-01-06 09:53:32 +0000

    Get rid of cppcheck warnings

--- Plugin-Backup/BackupPlugin.cc
+++ Plugin-Backup/BackupPlugin.cc
@@ -322,7 +322,7 @@ void BackupPlugin::slotCreateBackup( IdList _objectids , QString _name, std::vec
   }
 
   //add global backup
-  if ( groupIds.size() > 0 ){
+  if ( ! groupIds.size().empty() ){
     
     GroupBackup* backup = new GroupBackup(groupIds, _name);
     globalBackup_.storeBackup( backup );
