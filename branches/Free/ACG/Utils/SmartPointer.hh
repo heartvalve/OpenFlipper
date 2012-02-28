dc8ee07d9fa1a28e3a79a80c8395a0e32f186c1e
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-02-28 09:14:50 +0000

    Added smartpointer header and wrapper file

---
+++ branches/Free/ACG/Utils/SmartPointer.hh
@@ -0,0 +1,85 @@
+/*===========================================================================*\
+ *                                                                           *
+ *                              OpenFlipper                                  *
+ *      Copyright (C) 2001-2011 by Computer Graphics Group, RWTH Aachen      *
+ *                           www.openflipper.org                             *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *  This file is part of OpenFlipper.                                        *
+ *                                                                           *
+ *  OpenFlipper is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
+ *                                                                           *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
+ *                                                                           *
+ *  OpenFlipper is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with OpenFlipper. If not,                                  *
+ *  see <http://www.gnu.org/licenses/>.                                      *
+ *                                                                           *
+\*===========================================================================*/
+
+/*===========================================================================*\
+ *                                                                           *
+ *   $Revision$                                                         *
+ *   $Author$                                                      *
+ *   $Date$                   *
+ *                                                                           *
+\*===========================================================================*/
+
+#ifndef SMARTPOINTER_HH
+#define SMARTPOINTER_HH
+
+
+//== FORWARDDECLARATIONS ======================================================
+
+//== NAMESPACES ===============================================================
+
+//== DEFINITION =========================================================
+
+/** This set of defines maps the pointer namespaces to the namespace ptr depending on
+ *  the current architecture and compilers.
+ */
+#if (__cplusplus >= 201103L)
+   // C++11:
+   #include <memory>
+   namespace ptr = std;
+   #define ACG_UNIQUE_POINTER_SUPPORTED 1
+#elif defined(__GXX_EXPERIMENTAL_CXX0X__)
+   // C++11 via -std=c++0x on gcc:
+   #include <memory>
+   namespace ptr = std;
+   #define ACG_UNIQUE_POINTER_SUPPORTED 1
+#else
+   // C++98 and TR1:
+   #if (_MSC_VER >= 1600)
+     // VStudio 2010 supports some C++11 features
+     #include <memory>
+     namespace ptr = std;
+     #define ACG_UNIQUE_POINTER_SUPPORTED 1
+   #else
+     // hope for TR1 equivalents
+     #include <tr1/memory>
+     namespace ptr = std::tr1;
+     #define ACG_UNIQUE_POINTER_SUPPORTED 0
+   #endif
+#endif
+
+
+//=============================================================================
+#endif // SMARTPOINTER_HH defined
+//=============================================================================
+
