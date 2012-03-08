67c8a5e00684793469b6c0e303b606257b0234ee
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-03-08 16:58:14 +0000

    Fixed gmm finder to use newer version of gmm

--- branches/Free/cmake/FindGMM.cmake
+++ branches/Free/cmake/FindGMM.cmake
@@ -14,13 +14,15 @@ GET_FILENAME_COMPONENT(module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
 
 # Look for the header file.
 FIND_PATH(GMM_INCLUDE_DIR NAMES gmm/gmm.h 
-                           PATHS /ACG/acgdev/gcc-4.3-i686/gmm-3/include/
+                           PATHS /usr/include
+                                 /ACG/acgdev/gcc-4.3-i686/gmm-4.1/include/
+                                 /ACG/acgdev/gcc-4.3-i686/gmm-3/include/
                                  /usr/include
                                  /usr/local/include
                                  ~/sw/gmm-4.1/include
+				 "c:\\libs\\gmm-4.1\\include"
                                  "c:\\libs\\gmm-3.0\\include"
-								 "c:\\libs\\gmm-4.1\\include"
-				                 ${PROJECT_SOURCE_DIR}/MacOS/Libs/gmm-3.1/include
+			         ${PROJECT_SOURCE_DIR}/MacOS/Libs/gmm-3.1/include
                                  ../../External/include
                                  ${module_file_path}/../../../External/include)
 
