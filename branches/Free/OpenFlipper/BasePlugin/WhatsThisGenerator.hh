67e228145515f3af837d65f73acc0ab7c6be2422
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-03-08 11:32:21 +0000

    Build on windows again, as the DLLEXPORT was missing for the new generator Fixed documentation bug Fixed tabs used for indent

--- branches/Free/OpenFlipper/BasePlugin/WhatsThisGenerator.hh
+++ branches/Free/OpenFlipper/BasePlugin/WhatsThisGenerator.hh
@@ -46,34 +46,40 @@
 #include <QAction>
 #include <QString>
 #include <QObject>
+#include <OpenFlipper/common/GlobalDefines.hh>
 
 /** \brief a class which provides an link generator for WhatsThisMessages linking to the user doc
  *	If you have an QAction pointer called action, you can call:
- *	 \code{.cpp}action->setWhatsThis("Hello");\encode
+ *	 \code{.cpp}action->setWhatsThis("Hello");\endcode
  *	This message "Hello" will be shown when somebody call this action in WhatsThisMode.
  *
  *	WhatsThisGenerator provides functions linking to the user documentation.
  *		e.g. assume that whatsThisGen is a valid WhatsThisGenerator object
- *		\code{.cpp}action->setWhatsThis(QString(tr("Hello"))+whatsThisGen.generateLink()); \encode
+ *		\code{.cpp}action->setWhatsThis(QString(tr("Hello"))+whatsThisGen.generateLink()); \endcode
  *	The code above will change the output for WhatsThisMessages to: "Hello Click here for more information"
  *	where "Click here for more information" is a link which will call the index.html (default parameter) of the plugin
  *	which you have to specify in the constructor of WhatsThisGenerator.
  *
  *	You can also use the WhatsThisGenerator to set the WhatsThisMessage:
- *		\code{.cpp}whatsThisGen.setWHatsThis(action,tr("Hello"));\encode
+ *		\code{.cpp}whatsThisGen.setWHatsThis(action,tr("Hello"));\endcode
  *	It has the same effect and options like the code above and is shorter.
  */
-class WhatsThisGenerator : public QObject
-{
-	Q_OBJECT;
-	const QString plugin_name_;
+class DLLEXPORT WhatsThisGenerator: public QObject {
+
+  Q_OBJECT
+
+  const QString plugin_name_;
 
 public:
-	WhatsThisGenerator(const QString &_plugin_name);
+  WhatsThisGenerator(const QString &_plugin_name);
 
-	QString generateLink(const QString &_ref = "",const QString &_site = "index.html") const;
+  QString generateLink(const QString &_ref = "", const QString &_site = "index.html") const;
 
-	void setWhatsThis(QAction* _action, const QString &_msg, const QString &_ref = "",const QString &_site = "index.html") const;
+  void setWhatsThis(
+      QAction* _action,
+      const QString &_msg,
+      const QString &_ref = "",
+      const QString &_site = "index.html") const;
 
 };
 
