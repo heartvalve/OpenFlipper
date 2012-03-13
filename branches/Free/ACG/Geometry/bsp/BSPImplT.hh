fabe4e361b0d8e487c9776600c4d8d9a38f75b4c
ebke <ebke@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-03-13 14:35:34 +0000

    Updated BSP Tree implementation with the OpenFlipper/common/bsp implementation which seemed newer (and better) to me.

--- branches/Free/ACG/Geometry/bsp/BSPImplT.hh
+++ branches/Free/ACG/Geometry/bsp/BSPImplT.hh
@@ -1,33 +1,44 @@
-//=============================================================================
-//
-//                               OpenFlipper
-//        Copyright (C) 2008 by Computer Graphics Group, RWTH Aachen
-//                           www.openflipper.org
-//
-//-----------------------------------------------------------------------------
-//
-//                                License
-//
-//  OpenFlipper is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-// 
-//  OpenFlipper is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-// 
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
-//
-//-----------------------------------------------------------------------------
-//
-//   $Revision: 1720 $
-//   $Author: moebius $
-//   $Date: 2008-05-09 14:15:53 +0200 (Fri, 09 May 2008) $
-//
-//=============================================================================
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2011 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision: 10745 $                                                       *
+*   $LastChangedBy: moebius $                                                *
+*   $Date: 2011-01-26 10:23:50 +0100 (Wed, 26 Jan 2011) $                     *
+*                                                                            *
+\*===========================================================================*/
 
 
 
@@ -49,6 +60,7 @@
 
 
 //== CLASS DEFINITION =========================================================
+#include <vector>
 
 
 template <class BSPCore>
@@ -79,11 +91,22 @@ public: //---------------------------------------------------------------------
     Handle  handle;
     Scalar  dist;
   };
+  
+  /// Store nearest neighbor information
+  struct RayCollision
+  {
+    RayCollision() {}
+    RayCollision(Handle _h, Scalar _d, std::vector<Handle> _v) : handle(_h), dist(_d), hit_vertices(_v) {}
+    Handle  handle;
+    Scalar  dist;
+    std::vector<Handle> hit_vertices;
+  };
 
-
-  /// Return handle of the nearest neighbor
+  /// Return handle of the nearest neighbor face
   NearestNeighbor nearest(const Point& _p) const;
-
+  
+  /// Return handles of all hit collided faces, and the handle of the nearest collided face
+  RayCollision raycollision (const Point& _p, const Point& _r) const;
   
 
 private: //---------------------------------------------------------------------
@@ -96,10 +119,23 @@ private: //---------------------------------------------------------------------
     Scalar  dist;
     Handle  nearest;
   };
+  
+  /// Store ray collide information
+  struct RayCollisionData
+  {
+    Point   ref;
+    Point   ray;
+    Scalar  dist;
+    Handle  nearest;
+    std::vector<Handle> hit_vertices;
+  };
 
 
   // Recursive part of nearest()
   void _nearest(Node* _node, NearestNeighborData& _data) const;
+  
+  //resursive part of raycollide()
+  void _raycollision(Node* _node, RayCollisionData& _data) const;
 };
 
 
--- branches/Free/ACG/Geometry/bsp/TriangleBSPT.hh
+++ branches/Free/ACG/Geometry/bsp/TriangleBSPT.hh
@@ -1,33 +1,44 @@
-//=============================================================================
-//
-//                               OpenFlipper
-//        Copyright (C) 2008 by Computer Graphics Group, RWTH Aachen
-//                           www.openflipper.org
-//
-//-----------------------------------------------------------------------------
-//
-//                                License
-//
-//  OpenFlipper is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-// 
-//  OpenFlipper is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-// 
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with OpenFlipper.  If not, see <http://www.gnu.org/licenses/>.
-//
-//-----------------------------------------------------------------------------
-//
-//   $Revision: 1720 $
-//   $Author: moebius $
-//   $Date: 2008-05-09 14:15:53 +0200 (Fri, 09 May 2008) $
-//
-//=============================================================================
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2011 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision: 10745 $                                                       *
+*   $LastChangedBy: moebius $                                                *
+*   $Date: 2011-01-26 10:23:50 +0100 (Wed, 26 Jan 2011) $                     *
+*                                                                            *
+\*===========================================================================*/
 
 
 
@@ -44,6 +55,7 @@
 
 //== INCLUDES =================================================================
 
+#include "BSPTreeNode.hh"
 #include "TriangleBSPCoreT.hh"
 #include "BSPImplT.hh"
 //#include "Distance.hh"
@@ -51,7 +63,7 @@
 
 
 //== CLASS DEFINITION =========================================================
-
+#include <list>
 
 template <class BSPTraits>
 class TriangleBSPT : public BSPImplT< TriangleBSPCoreT<BSPTraits> >
@@ -61,39 +73,121 @@ public:
   TriangleBSPT(const BSPTraits& _traits) : Base(_traits) {}
 };
 
-
 //== CLASS DEFINITION =========================================================
-
 
 template <class Mesh>
 class OpenMeshTriangleBSPTraits
 {
 public:
 
-  typedef typename Mesh::Scalar      Scalar;
-  typedef typename Mesh::Point       Point;
-  typedef typename Mesh::FaceHandle  Handle;
+    typedef typename Mesh::Scalar	Scalar;
+    typedef typename Mesh::Point	Point;
+    typedef typename Mesh::FaceHandle	Handle;
+    typedef std::vector<Handle>		Handles;
+    typedef typename Handles::iterator	HandleIter;
+    typedef TreeNode<Mesh>		Node;    
+     
+    OpenMeshTriangleBSPTraits(const Mesh& _mesh) : mesh_(_mesh) {}
 
-  OpenMeshTriangleBSPTraits(const Mesh& _mesh) : mesh_(_mesh) {}
+    /// Returns the points belonging to the face handle _h
+    inline void points(const Handle _h, Point& _p0, Point& _p1, Point& _p2) const
+    {
+        typename Mesh::CFVIter fv_it(mesh_.cfv_iter(_h));
+        _p0 = mesh_.point(fv_it);
+        ++fv_it;
+        _p1 = mesh_.point(fv_it);
+        ++fv_it;
+        _p2 = mesh_.point(fv_it);
+    }
 
-  void points(Handle _h, Point& _p0, Point& _p1, Point& _p2) const 
-  { 
-    typename Mesh::CFVIter fv_it(mesh_.cfv_iter(_h));
-    _p0 = mesh_.point(fv_it); ++fv_it;
-    _p1 = mesh_.point(fv_it); ++fv_it;
-    _p2 = mesh_.point(fv_it);
-  }
+    Scalar sqrdist(const Handle _h, const Point& _p) const
+    {
+        Point p0, p1, p2, q;
+        points(_h, p0, p1, p2);
+        return ACG::Geometry::distPointTriangleSquared(_p, p0, p1, p2, q);
+    }
+    
+    void calculateBoundingBox(
+        Node* _node,
+        Point& median,
+	int& axis)
+    {	
+	//determine splitting axis
+	HandleIter it_h;
+        Point p0, p1, p2, bb_min, bb_max;
+        bb_min.vectorize(FLT_MAX);
+        bb_max.vectorize(-FLT_MAX);
+	std::list<Point> vertices;
+	
+	
+        for (it_h=_node->begin(); it_h!=_node->end(); ++it_h)
+        {
+            points(*it_h, p0, p1, p2);
+            /*
+	    bb_min.minimize(p0);
+            bb_min.minimize(p1);
+            bb_min.minimize(p2);
+            bb_max.maximize(p0);
+            bb_max.maximize(p1);
+            bb_max.maximize(p2);*/
+	    
+	    vertices.push_back (p0);
+	    vertices.push_back (p1);
+	    vertices.push_back (p2);
+        }
+	bb_min = _node->bb_min;
+	bb_max = _node->bb_max;
+	
+        // split longest side of bounding box
+        Point   bb     = bb_max - bb_min;
+        Scalar  length = bb[0];
+	axis = 0;
+        if (bb[1] > length) length = bb[ (axis=1) ];
+        if (bb[2] > length) length = bb[ (axis=2) ];
+	
+	//calculate the median value in axis-direction
+	switch (axis) {
+	  case 0: vertices.sort (x_sort() ); break;
+	  case 1: vertices.sort (y_sort() ); break;
+	  case 2: vertices.sort (z_sort() ); break; }
+	vertices.unique(); ///todo: does this work with Points?!
+	
+	int size = vertices.size();
+	typename std::list<Point>::iterator it_v;
+	it_v = vertices.begin();
+	std::advance(it_v, size/2);
+	median = *it_v;
+	
+    }
 
-  Scalar sqrdist(Handle _h, const Point& _p) const 
-  {
-    Point p0, p1, p2, q;
-    points(_h, p0, p1, p2);
-    return ACG::Geometry::distPointTriangleSquared(_p, p0, p1, p2, q);
-  }
+    void calculateBoundingBoxRoot(
+        Node* _node)
+    {
+        HandleIter it;
+        Point p0, p1, p2, bb_min, bb_max;
+        bb_min.vectorize(FLT_MAX);
+        bb_max.vectorize(-FLT_MAX);
+        for (it=_node->begin(); it!=_node->end(); ++it)
+        {
+            points(*it, p0, p1, p2);
+            bb_min.minimize(p0);
+            bb_min.minimize(p1);
+            bb_min.minimize(p2);
+            bb_max.maximize(p0);
+            bb_max.maximize(p1);
+            bb_max.maximize(p2);
+        }
+        _node->bb_min = bb_min;
+        _node->bb_max = bb_max;
+    }
 
 private:
 
-  const Mesh& mesh_;
+    const Mesh& mesh_;
+    //functors for sorting in different directions
+    struct x_sort { bool operator()(const Point& first, const Point& second) { return (first[0] < second[0]); }  };
+    struct y_sort { bool operator()(const Point& first, const Point& second) { return (first[1] < second[1]); }  };
+    struct z_sort { bool operator()(const Point& first, const Point& second) { return (first[2] < second[2]); }  };
 };
 
 
@@ -109,7 +203,6 @@ public:
   typedef TriangleBSPT<Traits>             Base;
   OpenMeshTriangleBSPT(const Mesh& _mesh) : Base(Traits(_mesh)) {}
 };
-
 
 //=============================================================================
 #endif // MB_TRIANGLEBSP_HH defined
