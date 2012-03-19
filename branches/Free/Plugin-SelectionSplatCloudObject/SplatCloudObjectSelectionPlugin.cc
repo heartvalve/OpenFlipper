62004d54bc2d20324dcc3b94733c6a8018da3451
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-03-19 12:36:41 +0000

    Headers,properties and some cleanup

--- branches/Free/Plugin-SelectionSplatCloudObject/SplatCloudObjectSelectionPlugin.cc
+++ branches/Free/Plugin-SelectionSplatCloudObject/SplatCloudObjectSelectionPlugin.cc
@@ -1,3 +1,45 @@
+/*===========================================================================*\
+ *                                                                           *
+ *                              OpenFlipper                                  *
+ *      Copyright (C) 2001-2010 by Computer Graphics Group, RWTH Aachen      *
+ *                           www.openflipper.org                             *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *  This file is part of OpenFlipper.                                        *
+ *                                                                           *
+ *  OpenFlipper is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as           *
+ *  published by the Free Software Foundation, either version 3 of           *
+ *  the License, or (at your option) any later version with the              *
+ *  following exceptions:                                                    *
+ *                                                                           *
+ *  If other files instantiate templates or use macros                       *
+ *  or inline functions from this file, or you compile this file and         *
+ *  link it with other files to produce an executable, this file does        *
+ *  not by itself cause the resulting executable to be covered by the        *
+ *  GNU Lesser General Public License. This exception does not however       *
+ *  invalidate any other reasons why the executable file might be            *
+ *  covered by the GNU Lesser General Public License.                        *
+ *                                                                           *
+ *  OpenFlipper is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU LesserGeneral Public          *
+ *  License along with OpenFlipper. If not,                                  *
+ *  see <http://www.gnu.org/licenses/>.                                      *
+ *                                                                           *
+\*===========================================================================*/
+
+/*===========================================================================*\
+ *                                                                           *
+ *   $Revision$                                                      *
+ *   $Author$                                                       *
+ *   $Date$                   *
+ *                                                                           *
+\*===========================================================================*/
+
 //================================================================
 //
 //  CLASS SplatCloudObjectSelectionPlugin - IMPLEMENTATION
@@ -473,6 +515,7 @@ void SplatCloudObjectSelectionPlugin::slotLoadSelection( const INIFile &_file )
 		// Read section for each object
 		// Append object name to section identifier
 		QString section = QString( "SplatCloudObjectSelection" ) + "//" + o_it->name();
+
 		if( !_file.section_exists( section ) )
 			continue;
 
@@ -500,10 +543,6 @@ void SplatCloudObjectSelectionPlugin::slotSaveSelection( INIFile &_file )
 		// Create section for each object
 		// Append object name to section identifier
 		QString section = QString( "SplatCloudObjectSelection" ) + "//" + o_it->name();
-		if( !_file.section_exists( section ) )
-			_file.add_section( section );
-		else
-			continue;
 
 		// Store vertex selection:
 		_file.add_entry( section, "VertexSelection", getVertexSelection( o_it->id() ) );
