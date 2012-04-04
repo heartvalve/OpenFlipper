b0a377df70890ecdee0e45cdefb6349090ca3140
lim <isaak.lim@cs.rwth-aachen.de>
2012-04-04 12:27:58 +0000

    It is now possible to provide run_single_object_file_mesh_test with an optional fourth argument, which gives a directory with meshes for testing. If a directory is given the TEST_FILE argument will be ignored.

--- tests/testGenerators.cmake
+++ tests/testGenerators.cmake
@@ -9,66 +9,139 @@
 # If that directory or file does not exist, no test will be generated!
 #
 # The third parameter is the script that will be used. It will be taken from the global tests directory
+#
+# An optional fourth parameter can be provided specifying a whole directory with meshes.
+# The TEST_FILE will be ignored and tests will be run on the meshes in the given directory.
+# For now the directory will not be traversed recursively.
 # TODO: also take local scripts!
 #
 # Used files:
 # fileMeshTest.ofs ( configured and copied script for meshes )
 # run_file_test.cmake ( Executing the test and comparing the results)
-function( run_single_object_file_mesh_test FILEPLUGIN TEST_FILE TEST_SCRIPT  )
+function( run_single_object_file_mesh_test FILEPLUGIN TEST_FILE TEST_SCRIPT )
 
   if ( NOT EXISTS ${CMAKE_SOURCE_DIR}/TestData)
       return()
   endif()
 
-  if ( NOT EXISTS ${CMAKE_SOURCE_DIR}/TestData/${TEST_FILE} )
-      return()
-  endif()
-
-  configure_file(${CMAKE_SOURCE_DIR}/TestData/${TEST_FILE}
-    ${OPENFLIPPER_TEST_FILES}/${TEST_FILE} COPYONLY)
-
   # construct the testname from target test file and the plugin directory we are in
   string (TOUPPER ${_plugin_dir} PLUGIN_DIR)
 
-  # replace possible forward slashes in TEST_FILE in order to avoid the creation
-  # of a new directory when the test script is configured
-  string (REPLACE "/" "-" TEST_FILE_NAME ${TEST_FILE})
-  set (TESTNAME "${PLUGIN_DIR}-${TEST_FILE_NAME}")
+  list( LENGTH ARGN opt_arg_length )
+  # handle a whole directory with test files
+  if ( opt_arg_length EQUAL 1 )
+    list(GET ARGN 0 dir)
 
-  set (TESTSCRIPTNAME "testscript-${TESTNAME}-${TEST_SCRIPT}")
+    if ( NOT EXISTS ${CMAKE_SOURCE_DIR}/TestData/${dir} )
+        return()
+    endif()
 
-  # update the output file to be inside the results directory
-  set (OUTPUT_TEST_DATA_FILE "${OPENFLIPPER_TEST_RESULTS}/${TESTSCRIPTNAME}-result.txt" )
+    # get the file ending (file type)
+    string( REPLACE "file" "" file_ending ${FILEPLUGIN} )
 
-  # set file containing the information about the file to be checked
-  set (TEST_FILE_INFO ${CMAKE_SOURCE_DIR}/TestData/${TEST_FILE}.info  )
+    file( GLOB files ${CMAKE_SOURCE_DIR}/TestData/${dir}/*.${file_ending} )
+    foreach( testfile ${files} )
 
-  # Configure the test script from the current directory with the given filenames and variables into the test directory
-  configure_file(${CMAKE_SOURCE_DIR}/tests/${TEST_SCRIPT}
-   ${CMAKE_BINARY_DIR}/tests/${_plugin_dir}/${TESTSCRIPTNAME} @ONLY )
+      # get the file name
+      file( RELATIVE_PATH filename ${CMAKE_SOURCE_DIR}/TestData/${dir} ${testfile} )
 
-  # Execute the script by OpenFlipper and than run the result parser which checks for correct values.
-  set( test_cmd ${OPENFLIPPER_EXECUTABLE} )
-  set( args "-c -b ${TESTSCRIPTNAME}" )
-  add_test( ${TESTNAME}
-     ${CMAKE_COMMAND}
-     -D test_cmd=${test_cmd}
-     -D test_args:string=${args}
-     -D output_test=${OUTPUT_TEST_DATA_FILE}
-     -D test_file_info=${TEST_FILE_INFO}
-     -D result_checker=${OPENFLIPPER_TEST_BINARIES}/compareTool
-     -P ${CMAKE_SOURCE_DIR}/tests/run_file_test.cmake
-  )
+      configure_file(${CMAKE_SOURCE_DIR}/TestData/${dir}/${filename}
+        ${OPENFLIPPER_TEST_FILES}/${dir}/${filename} COPYONLY)
 
-  # Timeout after 2 minutes if we have an endless loop
-  # Should be run serial to avoid collisons with other instances
-  # Only one processor required
-  set_tests_properties (
-      ${TESTNAME} PROPERTIES
-      TIMEOUT 120
-      RUN_SERIAL TRUE
-      PROCESSORS 1
-  )
+      # replace possible forward slashes in TEST_FILE in order to avoid the creation
+      # of a new directory when the test script is configured
+      string (REPLACE "/" "-" TEST_FILE_NAME ${filename})
+      set (TESTNAME "${PLUGIN_DIR}-${TEST_FILE_NAME}")
+
+      set (TESTSCRIPTNAME "testscript-${TESTNAME}-${TEST_SCRIPT}")
+
+      # update the output file to be inside the results directory
+      set (OUTPUT_TEST_DATA_FILE "${OPENFLIPPER_TEST_RESULTS}/${TESTSCRIPTNAME}-result.txt" )
+
+      # set file containing the information about the file to be checked
+      set (TEST_FILE_INFO ${testfile}.info  )
+
+      set (TEST_FILE ${dir}/${filename})
+
+      # Configure the test script from the current directory with the given filenames and variables into the test directory
+      configure_file(${CMAKE_SOURCE_DIR}/tests/${TEST_SCRIPT}
+       ${CMAKE_BINARY_DIR}/tests/${_plugin_dir}/${TESTSCRIPTNAME} @ONLY )
+
+      # Execute the script by OpenFlipper and than run the result parser which checks for correct values.
+      set( test_cmd ${OPENFLIPPER_EXECUTABLE} )
+      set( args "-c -b ${TESTSCRIPTNAME}" )
+      add_test( ${TESTNAME}
+         ${CMAKE_COMMAND}
+         -D test_cmd=${test_cmd}
+         -D test_args:string=${args}
+         -D output_test=${OUTPUT_TEST_DATA_FILE}
+         -D test_file_info=${TEST_FILE_INFO}
+         -D result_checker=${OPENFLIPPER_TEST_BINARIES}/compareTool
+         -P ${CMAKE_SOURCE_DIR}/tests/run_file_test.cmake
+      )
+
+      # Timeout after 2 minutes if we have an endless loop
+      # Should be run serial to avoid collisons with other instances
+      # Only one processor required
+      set_tests_properties (
+          ${TESTNAME} PROPERTIES
+          TIMEOUT 120
+          RUN_SERIAL TRUE
+          PROCESSORS 1
+      )
+    endforeach()
+
+  # handle a single test file
+  elseif( opt_arg_length EQUAL 0)
+
+    if ( NOT EXISTS ${CMAKE_SOURCE_DIR}/TestData/${TEST_FILE} )
+        return()
+    endif()
+
+    configure_file(${CMAKE_SOURCE_DIR}/TestData/${TEST_FILE}
+      ${OPENFLIPPER_TEST_FILES}/${TEST_FILE} COPYONLY)
+
+    # replace possible forward slashes in TEST_FILE in order to avoid the creation
+    # of a new directory when the test script is configured
+    string (REPLACE "/" "-" TEST_FILE_NAME ${TEST_FILE})
+    set (TESTNAME "${PLUGIN_DIR}-${TEST_FILE_NAME}")
+
+    set (TESTSCRIPTNAME "testscript-${TESTNAME}-${TEST_SCRIPT}")
+
+    # update the output file to be inside the results directory
+    set (OUTPUT_TEST_DATA_FILE "${OPENFLIPPER_TEST_RESULTS}/${TESTSCRIPTNAME}-result.txt" )
+
+    # set file containing the information about the file to be checked
+    set (TEST_FILE_INFO ${CMAKE_SOURCE_DIR}/TestData/${TEST_FILE}.info  )
+
+    # Configure the test script from the current directory with the given filenames and variables into the test directory
+    configure_file(${CMAKE_SOURCE_DIR}/tests/${TEST_SCRIPT}
+     ${CMAKE_BINARY_DIR}/tests/${_plugin_dir}/${TESTSCRIPTNAME} @ONLY )
+
+    # Execute the script by OpenFlipper and than run the result parser which checks for correct values.
+    set( test_cmd ${OPENFLIPPER_EXECUTABLE} )
+    set( args "-c -b ${TESTSCRIPTNAME}" )
+    add_test( ${TESTNAME}
+       ${CMAKE_COMMAND}
+       -D test_cmd=${test_cmd}
+       -D test_args:string=${args}
+       -D output_test=${OUTPUT_TEST_DATA_FILE}
+       -D test_file_info=${TEST_FILE_INFO}
+       -D result_checker=${OPENFLIPPER_TEST_BINARIES}/compareTool
+       -P ${CMAKE_SOURCE_DIR}/tests/run_file_test.cmake
+    )
+
+    # Timeout after 2 minutes if we have an endless loop
+    # Should be run serial to avoid collisons with other instances
+    # Only one processor required
+    set_tests_properties (
+        ${TESTNAME} PROPERTIES
+        TIMEOUT 120
+        RUN_SERIAL TRUE
+        PROCESSORS 1
+    )
+  endif()
+
 endfunction()
 
 
