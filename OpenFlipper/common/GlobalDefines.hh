bf3fb053d2f9d1bee7f12c5dc2a86df2fc838a31
moebius <moebius@informatik.rwth-aachen.de>
2012-04-26 14:54:45 +0000

    Updated qt tested version to 4.8.1

--- OpenFlipper/common/GlobalDefines.hh
+++ OpenFlipper/common/GlobalDefines.hh
@@ -67,12 +67,12 @@
 #define GCC_VERSION (__GNUC__ * 1000 + __GNUC_MINOR__ * 10)
 
 
-#if QT_VERSION > 0x040800
+#if QT_VERSION > 0x040801
   #ifdef WIN32
-    #pragma message("Detected QT Version higher than 4.8!")
+    #pragma message("Detected QT Version higher than 4.8.1!")
     #pragma message("OpenFlipper has not been tested with this QT Version.")
   #else
-    #warning Detected QT Version higher than 4.8!
+    #warning Detected QT Version higher than 4.8.1!
     #warning OpenFlipper has not been tested with this QT Version.
   #endif
 #endif
