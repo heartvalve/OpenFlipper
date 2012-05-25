25255961ebd25b2600cb4e11fcfe23a925265b3b
ebke <ebke@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-05-25 09:58:41 +0000

    Algorithm/DBSCAN: Improved and fixed DBSCAN.

--- branches/Free/ACG/Algorithm/DBSCANT.hh
+++ branches/Free/ACG/Algorithm/DBSCANT.hh
@@ -30,6 +30,7 @@ namespace Algorithm {
  *
  * Returned cluster indices are guaranteed to be a continuous range starting at 1.
  *
+ *
  * Result has to support the operation `*result++ = <int>`.
  *
  * Example:
@@ -46,11 +47,13 @@ namespace Algorithm {
  * @param result Output iterator to the initial position of the result range. The range includes as many elements as [first, last).
  * @param epsilon The density-reachable neighborhood radius.
  * @param n_min The density-reachable count threshold.
+ * @param weight_func Unary function taking an element as its argument. Returns the weight with which it is to be considered for DBSCAN.
+ *   The off the shelf DBSCAN uses a constant 1, here.
  * @return The number of clusters found.
  */
-template<typename INPUT_ITERATOR, typename DISTANCE_FUNC, typename OUTPUT_ITERATOR>
+template<typename INPUT_ITERATOR, typename DISTANCE_FUNC, typename OUTPUT_ITERATOR, typename WEIGHT_FUNC>
 int DBSCAN(const INPUT_ITERATOR first, const INPUT_ITERATOR last, DISTANCE_FUNC distance_func,
-           OUTPUT_ITERATOR result, const double epsilon, const int n_min) {
+           OUTPUT_ITERATOR result, const double epsilon, const double n_min, WEIGHT_FUNC weight_func) {
 
     const size_t input_size = std::distance(first, last);
 
@@ -68,18 +71,27 @@ int DBSCAN(const INPUT_ITERATOR first, const INPUT_ITERATOR last, DISTANCE_FUNC 
         std::vector<INPUT_ITERATOR> neighborhood; neighborhood.reserve(input_size);
         _DBSCAN_PRIVATE::region_query(first, last, it, distance_func, std::back_inserter(neighborhood), epsilon);
 
-        if ((int)neighborhood.size() < n_min) {
+        if (_DBSCAN_PRIVATE::neighborhoodWeight(neighborhood.begin(), neighborhood.end(), weight_func) < n_min) {
             // It's noise.
             id_cache[idx] = 0;
         } else {
             // It's the seed of a cluster.
-            _DBSCAN_PRIVATE::expand_cluster(first, last, it, distance_func, epsilon, n_min, id_cache, ++current_cluster_id);
+            _DBSCAN_PRIVATE::expand_cluster(first, last, it, distance_func, epsilon, n_min, id_cache, ++current_cluster_id, weight_func);
         }
     }
 
     std::copy(id_cache.begin(), id_cache.end(), result);
 
     return current_cluster_id;
+}
+
+/**
+ * Version of DBSCAN with weight_func being a constant 1.
+ */
+template<typename INPUT_ITERATOR, typename DISTANCE_FUNC, typename OUTPUT_ITERATOR>
+int DBSCAN(const INPUT_ITERATOR first, const INPUT_ITERATOR last, DISTANCE_FUNC distance_func,
+           OUTPUT_ITERATOR result, const double epsilon, const double n_min) {
+    return DBSCAN(first, last, distance_func, result, epsilon, n_min, typename _DBSCAN_PRIVATE::constant_1<typename INPUT_ITERATOR::value_type>());
 }
 
 } /* namespace Algorithm */
