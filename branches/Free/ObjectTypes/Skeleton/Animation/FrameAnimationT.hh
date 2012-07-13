7d7cecf635786f79ca3115d2e348a9a21cbc07ff
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-07-13 14:03:02 +0000

    Next documentation try

--- branches/Free/ObjectTypes/Skeleton/Animation/FrameAnimationT.hh
+++ branches/Free/ObjectTypes/Skeleton/Animation/FrameAnimationT.hh
@@ -72,22 +72,44 @@ class FrameAnimationT : public AnimationT<PointT>
     /**
       * @name Frame access
       * There is one pose per frame.
-      */
-    //@{
+       * @{ */
+
+    /** \brief Returns a pointer to the pose stored in the given frame
+     *
+     * @param _iFrame The poses frame number
+     */
     inline Pose *pose(unsigned int _iFrame);
+
+    /** \brief Returns the number of frames stored in this pose
+     */
     inline unsigned int frameCount();
 
+    /** \brief Set number of frames stored in this pose
+     */
     void setFrameCount(unsigned int _frames);
-    //@}
+    /** @} */
 
-    /**
-      * @name Synchronization
+    /** @name Synchronization
+      *
       * Use these methods to keep the poses in sync with the number (and indices) of the joints.
-      */
-    //@{
+      * @{ */
+
+    /** \brief Called by the skeleton as a new joint is inserted
+     *
+     * The call is dispatched to all poses stored in this animation. See BasePoseT::insert_at for more information.
+     *
+     * @param _index The new joint is inserted at this position. Insert new joints at the end by passing SkeletonT::joints_.size as parameter.
+     */
     void insertJointAt(unsigned int _index);
+
+    /** \brief Called by the skeleton as a joint is deleted
+     *
+     * The call is dispatched to all poses stored in this animation. See BasePoseT::remove_at for more information.
+     *
+     * @param _index The index of the joint that is being deleted.
+     */
     void removeJointAt(unsigned int _index);
-    //@}
+    /** @} */
 
   private:
     /// Pointer to associated skeleton
