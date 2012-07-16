ca5519007bf8113f0eae4ca54c155088a0f1c9f3
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-07-16 11:26:20 +0000

    Fixed cppcheck warning

--- branches/Free/Plugin-SelectionMeshObject/MeshObjectSelectionPlugin.hh
+++ branches/Free/Plugin-SelectionMeshObject/MeshObjectSelectionPlugin.hh
@@ -207,21 +207,21 @@ public slots:
 
     // Is vertex type active? (for use in plugins that need mesh selection)
     bool vertexTypeActive() {
-        SelectionInterface::PrimitiveType t;
+        SelectionInterface::PrimitiveType t = 0u;
         emit getActivePrimitiveType(t);
         return (t & vertexType_) > 0;
     }
 
     // Is vertex type active? (for use in plugins that need mesh selection)
     bool edgeTypeActive() {
-        SelectionInterface::PrimitiveType t;
+        SelectionInterface::PrimitiveType t = 0u;
         emit getActivePrimitiveType(t);
         return (t & edgeType_) > 0;
     }
 
     // Is face type active? (for use in plugins that need mesh selection)
     bool faceTypeActive() {
-        SelectionInterface::PrimitiveType t;
+        SelectionInterface::PrimitiveType t = 0u;
         emit getActivePrimitiveType(t);
         return (t & faceType_) > 0;
     }
