e6e22e305ac8a23df22e823ac65eabfbfd96859d
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-07-18 13:03:30 +0000

    Added license headers and fixed smaller typos. Added properties

--- branches/Free/Plugin-FileBundle/FileBundle.hh
+++ branches/Free/Plugin-FileBundle/FileBundle.hh
@@ -1,8 +1,53 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2011 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision$                                                       *
+*   $LastChangedBy$                                                *
+*   $Date$                     *
+*                                                                            *
+\*===========================================================================*/
+
+
 //================================================================
 //
 //  CLASS FileBundlePlugin
 //
-//    This class is the base class for loading and saving (reading/writing) SplatCloud objects from/to disc.
+//    This class is the base class for loading and saving
+//    (reading/writing) SplatCloud objects from/to bundler files
+//    on disc.
 //
 //================================================================
 
@@ -96,7 +141,7 @@ private:
 
   typedef std::vector<std::string> StringVector;
 
-  // read imagelist file from disc
+  // read image list file from disc
   bool readImagelistFile( const char *_filename, StringVector &_imagePaths ) /*const*/;
 
   // read bundle file from disc to scenegraph node
