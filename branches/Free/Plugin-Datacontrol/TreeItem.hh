cc27c669711faf7159e32817583ca57c1a5c3632
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2012-07-31 13:56:22 +0000

    Tobias: Speedup datacontrol

--- branches/Free/Plugin-Datacontrol/TreeItem.hh
+++ branches/Free/Plugin-Datacontrol/TreeItem.hh
@@ -110,11 +110,17 @@ class TreeItem {
     int level();
 
   private:
-    /// Parent item or 0 if rootnode
+    /// Parent item or 0 if root node
     TreeItem *parentItem_;
+
+    /// Index of this node in parent's childen
+    int row_;
 
     /// Children of this node
     QList<TreeItem*> childItems_;
+
+    /// Acceleration map
+    static QMap<int,TreeItem*> kTreeMap_;
 
   public:
     //===========================================================================
@@ -125,7 +131,7 @@ class TreeItem {
     /// get the row of this item from the parent
     int row() const;
 
-    /// Get the parent item ( 0 if rootitem )
+    /// Get the parent item ( 0 if root item )
     TreeItem *parent();
 
     /// Set the parent pointer
