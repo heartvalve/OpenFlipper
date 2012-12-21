2379c16321a4ede87b4be484757eeb1456d83865
moebius <moebius@informatik.rwth-aachen.de>
2012-12-21 12:16:28 +0000

    Tried to get rid of the fixbundle calls

--- OpenFlipper/cmake/package.cmake
+++ OpenFlipper/cmake/package.cmake
@@ -262,6 +262,13 @@ if (NOT WIN32 AND NOT APPLE)
   file (REMOVE "${CMAKE_BINARY_DIR}/CPackConfig.cmake")
 endif ()
 
+
+#Add dependency to fixbundle on APPLE when creating the package
+if ( APPLE )
+  add_dependencies(package fixbundle)
+endif()
+
+
 # cmake doesn't create a source package target, so we have to add our own
 if (EXISTS "${CMAKE_BINARY_DIR}/CPackSourceConfig.cmake")
   add_custom_target (PACKAGE_SOURCE
