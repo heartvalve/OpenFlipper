acba7de715640d4c03b39f2efbe845c40d243d72
lim <isaak.lim@cs.rwth-aachen.de>
2013-01-25 12:34:39 +0000

    added a run_local_script_test function to testGenerators, which execture OpenFlipper test scripts:   * add call to CMakeLists.txt in tests directory of plugin:      run_local_script_test( script_name ) added run_script_test to execture all local OpenFlipper test scripts

---
+++ tests/run_script_test.cmake
@@ -0,0 +1,26 @@
+# some argument checking:
+# test_cmd is the command to run with all its arguments
+if( NOT test_cmd )
+   message( FATAL_ERROR "Variable test_cmd not defined" )
+endif( NOT test_cmd )
+
+# convert the space-separated string to a list
+separate_arguments( test_args )
+
+# ===================================================
+# Run OpenFlipper with the given script
+# ===================================================
+
+# Execute the process with the given arguments
+# Timeout after 5 minutes
+execute_process(
+   COMMAND ${test_cmd} ${test_args}
+   TIMEOUT 300
+   RESULT_VARIABLE PROCESSRESULT
+)
+
+if ( NOT ${PROCESSRESULT} EQUAL 0 )
+    message("Problem when executing proccess. Return code was ${PROCESSRESULT}")
+    message("Test command: ${test_cmd} ${test_args}")
+    message(SEND_ERROR "Process execution failed!")
+endif()
