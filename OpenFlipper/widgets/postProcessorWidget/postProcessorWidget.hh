286de05c3068b329401216c3dfa0250233fc0ace
moebius <moebius@informatik.rwth-aachen.de>
2013-01-31 13:03:56 +0000

    added postprocessor manager widget

---
+++ OpenFlipper/widgets/postProcessorWidget/postProcessorWidget.hh
@@ -0,0 +1,77 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2011 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision: 15910 $                                                       *
+*   $LastChangedBy: moeller $                                                *
+*   $Date: 2012-12-05 12:53:39 +0100 (Mi, 05 Dez 2012) $                     *
+*                                                                            *
+\*===========================================================================*/
+
+#include "ui_postProcessorWidget.hh"
+#include <QtGui>
+
+#include <OpenFlipper/common/RendererInfo.hh>
+
+class PostProcessorDialog : public QDialog, public Ui::PostProcessorWidget
+{
+  Q_OBJECT
+
+  public:
+    PostProcessorDialog(QWidget *parent = 0);
+
+
+
+  private slots:
+    /// Show the custom context menu
+    void slotContextMenu(const QPoint& _point);
+
+    /// Activates the post processor (triggered via the context menu)
+    void slotActivatePostProcessor();
+
+  protected:
+    void closeEvent(QCloseEvent *event);
+
+    void showEvent ( QShowEvent * );
+
+  private:
+
+    void update();
+
+    std::vector<QLabel*> descriptions_;
+    QVector< QFrame* > frames_;
+
+};
+
