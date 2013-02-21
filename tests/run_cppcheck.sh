a466bfb01568057cd839fe8bd8d7a1502194b04b
moebius <moebius@informatik.rwth-aachen.de>
2013-02-21 07:12:32 +0000

    Fixed cppcheck script

--- tests/run_cppcheck.sh
+++ tests/run_cppcheck.sh
@@ -1,13 +1,13 @@
-cppcheck  --xml  \\
-          --verbose \\
-          --quiet \\
-          -i libs_required/OpenMesh/src/OpenMesh/Apps/Unsupported \\
-          -i build-release \\
-          --force \\
-          -UDOXY_IGNORE_THIS \\
-          --suppress=unusedPrivateFunction \\
-          --suppress=missingInclude  \\
-          --suppress=ConfigurationNotChecked \\
-          --suppress=negativeIndex:Plugin-PoissonReconstruction/PoissonReconstruction/BSplineData.h:39 \\
+cppcheck  --xml  \
+          --verbose \
+          --quiet \
+          -i libs_required/OpenMesh/src/OpenMesh/Apps/Unsupported \
+          -i build-release \
+          --force \
+          -UDOXY_IGNORE_THIS \
+          --suppress=unusedPrivateFunction \
+          --suppress=missingInclude  \
+          --suppress=ConfigurationNotChecked \
+          --suppress=negativeIndex:Plugin-PoissonReconstruction/PoissonReconstruction/BSplineData.h:39 \
           --enable=all . 2> cppcheck-result.xml
 
