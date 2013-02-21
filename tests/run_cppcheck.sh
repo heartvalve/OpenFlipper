cc6961c2446004e98d76ee5e9c5ec500f5043471
moebius <moebius@informatik.rwth-aachen.de>
2013-02-21 08:20:44 +0000

    Disable the unused function check and run with 4 threads at once.

--- tests/run_cppcheck.sh
+++ tests/run_cppcheck.sh
@@ -3,11 +3,20 @@ cppcheck  --xml  \
           --quiet \
           -i libs_required/OpenMesh/src/OpenMesh/Apps/Unsupported \
           -i build-release \
+          -i build-debug \
+          -i .svn \
           --force \
           -UDOXY_IGNORE_THIS \
           --suppress=unusedPrivateFunction \
           --suppress=missingInclude  \
           --suppress=ConfigurationNotChecked \
           --suppress=negativeIndex:Plugin-PoissonReconstruction/PoissonReconstruction/BSplineData.h:39 \
-          --enable=all . 2> cppcheck-result.xml
+          --enable=style \
+          --enable=performance \
+          --enable=portability \
+          --enable=information \
+          --enable=missingInclude -j4 \
+          . 2> cppcheck-result.xml
 
+
+#--enable=all
