baf248e455c58b2ec56be7107e8d09c671a79551
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2013-03-05 12:54:51 +0000

    Updated release howto

---
+++ branches/Free/OpenFlipper/Documentation/DeveloperHelpSources/release.howto.sh
@@ -0,0 +1,8 @@
+
+echo "mac/Info.plist ========================================"
+cat ../../../OpenFlipper/installer/mac/Info.plist
+echo "VERSION ========================================"
+cat ../../../OpenFlipper/VERSION
+echo "changelog head ========================================"
+head -n5 ../../../OpenFlipper/Documentation/DeveloperHelpSources/changelog.docu
+
