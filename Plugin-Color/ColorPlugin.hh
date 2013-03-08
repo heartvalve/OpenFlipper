75e635e8e054defac39d8278838f6350a24cdb51
moebius <moebius@informatik.rwth-aachen.de>
2013-03-08 16:01:49 +0000

    Added scripting function to set color of object via color plugin

--- Plugin-Color/ColorPlugin.hh
+++ Plugin-Color/ColorPlugin.hh
@@ -181,6 +181,13 @@ class ColorPlugin : public QObject, BaseInterface, MenuInterface, ScriptInterfac
     */
     void setBackgroundColor( Vector _color );
 
+    /** Sets the emissive color of an object
+     *
+     * @param _objectId  Id of an Object
+     * @param _color     Color to be set
+     */
+    void setObjectEmissiveColor(int _objectId , Vector4 _color);
+
   /** @} */
 
   //===========================================================================
