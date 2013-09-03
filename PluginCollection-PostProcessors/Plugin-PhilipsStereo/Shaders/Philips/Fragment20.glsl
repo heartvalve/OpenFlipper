963364015b3c656b2e29d80b8bf9ec88cff000d5
moebius <moebius@informatik.rwth-aachen.de>
2013-09-03 14:28:01 +0000

    Fixed depth shaders

--- PluginCollection-PostProcessors/Plugin-PhilipsStereo/Shaders/Philips/Fragment20.glsl
+++ PluginCollection-PostProcessors/Plugin-PhilipsStereo/Shaders/Philips/Fragment20.glsl
@@ -1,25 +1,33 @@
+#version 150
 
 uniform sampler2D       ColorTexture;
 uniform sampler2D       DepthStencil;
 
 
+in vec2 vTexCoord;
+out vec4 oColor;
+
+vec2 coordinate;
+
 void main(void)
 {
+
+ coordinate = vTexCoord;
  
  // Left  side is the color image seen by the user
  // Right side is the the depth map of the current view
  // Texture coordinates have to be scaled
  // and for the left image they also have to be shifted
- if ( gl_TexCoord[0].s < 0.5 ) {
-   gl_TexCoord[0].s = gl_TexCoord[0].s * 2.0; 
-   gl_FragColor = texture2D( ColorTexture, gl_TexCoord[0].st );
+ if ( vTexCoord.x < 0.5 ) {
+   coordinate.x = vTexCoord.x *  2.0; 
+   oColor = texture2D( ColorTexture, coordinate );
  } else {
-   gl_TexCoord[0].s = (gl_TexCoord[0].s - 0.5) * 2.0;
-   vec4 convert = texture2D( DepthStencil, gl_TexCoord[0].st );
+   coordinate.x = (vTexCoord.x - 0.5) * 2.0;
+   vec4 convert = texture2D( DepthStencil, coordinate );
    
    // This conversion is specified in the docs for a 20'' Display
-   convert.rgba = (-1586.34 *( 1.0 - (6.180772 / ( convert.r - 0.459813 + 6.180772)) ) + 127.5) / 255.0;
-   gl_FragColor = convert;
+   convert = vec4((-1586.34 *( 1.0 - (6.180772 / ( convert.r - 0.459813 + 6.180772)) ) + 127.5) / 255.0);
+   oColor = convert;
  }
 
 }
