98da38a4b731771af35f87311c183301481f5e33
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2013-09-03 14:28:01 +0000

    Fixed depth shaders

--- branches/Free/PluginCollection-PostProcessors/Plugin-PhilipsStereo/Shaders/Philips/Fragment42.glsl
+++ branches/Free/PluginCollection-PostProcessors/Plugin-PhilipsStereo/Shaders/Philips/Fragment42.glsl
@@ -7,22 +7,26 @@ uniform sampler2D       DepthStencil;
 in vec2 vTexCoord;
 out vec4 oColor;
 
+vec2 coordinate;
+
 void main(void)
 {
+
+ coordinate = vTexCoord;
  
  // Left  side is the color image seen by the user
  // Right side is the the depth map of the current view
  // Texture coordinates have to be scaled
  // and for the left image they also have to be shifted
  if ( vTexCoord.x < 0.5 ) {
-   vTexCoord.x = vTexCoord.x *  2.0; 
-   oColor = texture2D( ColorTexture, vTexCoord );
+   coordinate.x = vTexCoord.x *  2.0; 
+   oColor = texture2D( ColorTexture, coordinate );
  } else {
-   vTexCoord.x = (vTexCoord.x - 0.5) * 2.0;
-   vec4 convert = texture2D( DepthStencil, vTexCoord );
+   coordinate.x = (vTexCoord.x - 0.5) * 2.0;
+   vec4 convert = texture2D( DepthStencil, coordinate );
    
    // This conversion is specified in the docs for a 42'' Display
-   convert.rgba = (-1960.37 *( 1.0 - (7.655192 / ( convert.r - 0.467481 + 7.655192)) ) + 127.5) / 255.0;
+   convert = vec4((-1960.37 *( 1.0 - (7.655192 / ( convert.r - 0.467481 + 7.655192)) ) + 127.5) / 255.0);
    oColor = convert;
  }
 
