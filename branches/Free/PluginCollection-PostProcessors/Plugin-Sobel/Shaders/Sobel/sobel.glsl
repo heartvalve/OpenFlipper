7334b44433e7c3b95093e093c37c92cbfd68d678
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2013-09-16 14:51:18 +0000

    Postprocessor to Free branch

---
+++ branches/Free/PluginCollection-PostProcessors/Plugin-Sobel/Shaders/Sobel/sobel.glsl
@@ -0,0 +1,34 @@
+#version 150
+
+uniform sampler2D textureSampler;
+
+in vec2 vTexCoord;
+out vec4 oColor;
+
+// The inverse of the texture dimensions along X and Y
+uniform vec2 texcoordOffset;
+
+void main() 
+{
+  vec2 coord_tl = vec2( vTexCoord.x + texcoordOffset.x , vTexCoord.y - texcoordOffset.y );
+  vec2 coord_t  = vec2( vTexCoord.x + texcoordOffset.x , vTexCoord.y                    );
+  vec2 coord_tr = vec2( vTexCoord.x + texcoordOffset.x , vTexCoord.y + texcoordOffset.y );
+  
+  vec2 coord_cl = vec2( vTexCoord.x,vTexCoord.y-texcoordOffset.y );
+  vec2 coord_cr = vec2( vTexCoord.x,vTexCoord.y+texcoordOffset.y );
+  
+  vec2 coord_bl = vec2( vTexCoord.x-texcoordOffset.x,vTexCoord.y-texcoordOffset.y );
+  vec2 coord_b  = vec2( vTexCoord.x-texcoordOffset.x,vTexCoord.y );
+  vec2 coord_br = vec2( vTexCoord.x-texcoordOffset.x,vTexCoord.y+texcoordOffset.y );
+  
+  
+  vec4 color_horizontal  = abs(texture2D(textureSampler, coord_tl)       - texture2D(textureSampler, coord_tr));
+       color_horizontal += abs(2.0 * texture2D(textureSampler, coord_cl) - 2.0 * texture2D(textureSampler, coord_cr));
+       color_horizontal += abs(texture2D(textureSampler, coord_bl) - texture2D(textureSampler, coord_br));
+       
+  vec4 color_vertical    = abs(texture2D(textureSampler, coord_tl)      - texture2D(textureSampler, coord_bl));
+       color_vertical   += abs(2.0 * texture2D(textureSampler, coord_t) - 2.0 * texture2D(textureSampler, coord_b));
+       color_vertical   += abs(texture2D(textureSampler, coord_tr)      - texture2D(textureSampler, coord_br));     
+       
+  oColor = color_horizontal + color_vertical;
+}
