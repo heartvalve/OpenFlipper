351c749b153ab658e8ab3dc345be13e26934fe28
moebius <moebius@informatik.rwth-aachen.de>
2013-09-16 07:07:04 +0000

    Moved Grayscale PostProcessor to Free branch

---
+++ PluginCollection-PostProcessors/Plugin-Grayscale/PostProcessorGrayPlugin.hh
@@ -0,0 +1,86 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2013 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision$                                                       *
+*   $LastChangedBy$                                                *
+*   $Date$                     *
+*                                                                            *
+\*===========================================================================*/
+
+
+#include <QObject>
+
+#include <OpenFlipper/BasePlugin/BaseInterface.hh>
+#include <OpenFlipper/BasePlugin/PostProcessorInterface.hh>
+
+#include <ACG/GL/globjects.hh>
+#include <ACG/ShaderUtils/GLSLShader.hh>
+
+class PostProcessorGrayscalePlugin : public QObject, BaseInterface, PostProcessorInterface
+{
+   Q_OBJECT
+   Q_INTERFACES(BaseInterface)
+   Q_INTERFACES(PostProcessorInterface)
+
+#if QT_VERSION >= 0x050000
+  Q_PLUGIN_METADATA(IID "org.OpenFlipper.Plugins.Plugin-PostProcessorGrayscale")
+#endif
+
+  public:
+   PostProcessorGrayscalePlugin();
+   ~PostProcessorGrayscalePlugin();
+  
+  public :
+    QString name() { return (QString("Grayscale Postprocessor Plugin")); };
+    QString description( ) { return (QString(tr("Converts an RGB image to grayscale"))); };
+
+    
+  public slots:
+    QString version() { return QString("1.0"); };
+    
+  private slots:
+
+    void postProcess(ACG::GLState* _glstate, const PostProcessorInput& _input, GLuint _targetFBO);
+
+    QString postProcessorName();
+
+  private:
+
+    /// shader
+    GLSL::Program* shader_;
+
+};
+
