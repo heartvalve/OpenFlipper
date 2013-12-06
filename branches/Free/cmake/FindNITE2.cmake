5b24ac1c0d0d8509e6555b9b63e02815655eb258
moeller <moeller@383ad7c9-94d9-4d36-a494-682f7c89f535>
2013-12-06 11:39:56 +0000

    extends nite2 & openni2 finders

--- branches/Free/cmake/FindNITE2.cmake
+++ branches/Free/cmake/FindNITE2.cmake
@@ -1,22 +1,40 @@
-if (NITE2_INCLUDE_DIR)
+# Try to find OPENMESH
+# Once done this will define
+#  
+# NITE2_FOUND           - system has NITE2
+# NITE2_INCLUDE_DIRS    - the NITE2 include directories
+# NITE2_LIBRARIES       - Link these to use NITE2
+#
+#=======================================
+
+if (NITE2_INCLUDE_DIR AND NITE2_LIBRARY)
   # in cache already
   set(NITE2_FOUND TRUE)
-else (NITE2_INCLUDE_DIR)
+else (NITE2_INCLUDE_DIR AND NITE2_LIBRARY)
 
 
-find_path(NITE2_INCLUDE_DIR NAMES NiTE.h
-	  PATHS /usr/local/OpenNI-Linux-x64-2.2/NiTE-Linux-x64-2.2/Include
-# 	  $ENV{NITE2_INCLUDE}
+  find_path(NITE2_INCLUDE_DIR NAMES NiTE.h
+	  PATHS 
+	  /usr/local/OpenNI-Linux-x64-2.2/NiTE-Linux-x64-2.2/Include
+	  "C:/Program Files/PrimeSense/NiTE2/Include" 
+#	  $ENV{NITE2_INCLUDE}
           )
 
-find_library( NITE2_LIBRARY 
-              NiTE2 
-              PATHS /usr/local/OpenNI-Linux-x64-2.2/NiTE-Linux-x64-2.2/Redist "C:/Program Files/OpenNI/Lib" $ENV{NITE2_REDIST64} )
+  find_library( NITE2_LIBRARY 
+              NITE2
+              PATHS 
+			  /usr/local/OpenNI-Linux-x64-2.2/NiTE-Linux-x64-2.2/Redist 
+			  "C:/Program Files/PrimeSense/NiTE2/Lib" 
+			  $ENV{NITE2_REDIST} )
 
-if (NITE2_INCLUDE_DIR)
-  # in cache already
-  set(NITE2_FOUND TRUE)
-endif(NITE2_INCLUDE_DIR)
+  if (NITE2_INCLUDE_DIR AND NITE2_LIBRARY) 
 
-endif(NITE2_INCLUDE_DIR)
+    set(NITE2_FOUND TRUE)
+    set(NITE2_INCLUDE_DIRS ${NITE2_INCLUDE_DIR} )
+	set(NITE2_LIBRARIES ${NITE2_LIBRARY} )
+	mark_as_advanced(NITE2_INCLUDE_DIR NITE2_LIBRARY)
+  
+  endif(NITE2_INCLUDE_DIR AND NITE2_LIBRARY)
+
+endif(NITE2_INCLUDE_DIR AND NITE2_LIBRARY)
 
--- branches/Free/cmake/FindOPENNI2.cmake
+++ branches/Free/cmake/FindOPENNI2.cmake
@@ -1,22 +1,40 @@
-if (OPENNI2_INCLUDE_DIR)
+# Try to find OPENMESH
+# Once done this will define
+#  
+# OPENNI2_FOUND           - system has OPENNI2
+# OPENNI2_INCLUDE_DIRS    - the OPENNI2 include directories
+# OPENNI2_LIBRARIES       - Link these to use OPENNI2
+#
+#=======================================
+
+if (OPENNI2_INCLUDE_DIR AND OPENNI2_LIBRARY)
   # in cache already
   set(OPENNI2_FOUND TRUE)
-else (OPENNI2_INCLUDE_DIR)
+else (OPENNI2_INCLUDE_DIR AND OPENNI2_LIBRARY)
 
 
-find_path(OPENNI2_INCLUDE_DIR NAMES OpenNI.h
-	  PATHS /usr/local/OpenNI-Linux-x64-2.2/Include
+  find_path(OPENNI2_INCLUDE_DIR NAMES OpenNI.h
+	  PATHS 
+	  /usr/local/OpenNI-Linux-x64-2.2/Include
+	  "C:/Program Files/OpenNI/Include" 
 	  $ENV{OPENNI2_INCLUDE}
           )
 
-find_library( OPENNI2_LIBRARY 
+  find_library( OPENNI2_LIBRARY 
               OpenNI2
-              PATHS /usr/local/OpenNI-Linux-x64-2.2/Redist "C:/Program Files/OpenNI/Lib" $ENV{OPENNI2_REDIST} )
+              PATHS 
+			  /usr/local/OpenNI-Linux-x64-2.2/Redist 
+			  "C:/Program Files/OpenNI/Lib" 
+			  $ENV{OPENNI2_REDIST} )
 
-if (OPENNI2_INCLUDE_DIR)
-  # in cache already
-  set(OPENNI2_FOUND TRUE)
-endif(OPENNI2_INCLUDE_DIR)
+  if (OPENNI2_INCLUDE_DIR AND OPENNI2_LIBRARY) 
 
-endif(OPENNI2_INCLUDE_DIR)
+    set(OPENNI2_FOUND TRUE)
+    set(OPENNI2_INCLUDE_DIRS ${OPENNI2_INCLUDE_DIR} )
+	set(OPENNI2_LIBRARIES ${OPENNI2_LIBRARY} )
+	mark_as_advanced(OPENNI2_INCLUDE_DIR OPENNI2_LIBRARY)
+  
+  endif(OPENNI2_INCLUDE_DIR AND OPENNI2_LIBRARY)
+
+endif(OPENNI2_INCLUDE_DIR AND OPENNI2_LIBRARY)
 
