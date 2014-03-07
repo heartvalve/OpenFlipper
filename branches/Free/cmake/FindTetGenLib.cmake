2518f9d07efe5079f12b1a0c784d04df719e75b9
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2014-03-07 08:00:56 +0000

    Added Tetgen Finder

---
+++ branches/Free/cmake/FindTetGenLib.cmake
@@ -0,0 +1,47 @@
+# - Try to find TetGenLib
+# Once done this will define
+#  
+#  TetGenLib_FOUND        - system has TetGenLib
+#  TetGenLib_INCLUDE_DIR  - the TetGenLib include directory
+#  TetGenLib_LIBRARY_DIR  - where the libraries are
+#  TetGenLib_LIBRARY      - Link these to use TetGenLib
+#   
+
+IF (TetGenLib_INCLUDE_DIR)
+  # Already in cache, be silent
+  SET(TetGenLib_FIND_QUIETLY TRUE)
+ENDIF (TetGenLib_INCLUDE_DIR)
+
+set(TetGenLib_TMP_PATHS /home/zimmer/projects/libs/)
+
+FIND_PATH( TetGenLib_INCLUDE_DIR tetgen.h
+  HINTS ${TetGenLib_TMP_PATHS}/TetGen/include/)
+
+if( TetGenLib_INCLUDE_DIR )
+  message( Found TetGenLib_INCLUDE_DIR: ${TetGenLib_INCLUDE_DIR})
+  find_library(
+    TetGenLib_LIBRARY
+    NAMES tetgen
+    PATHS ${TetGenLib_TMP_PATHS}/TetGen/lib/)
+  
+  if( TetGenLib_LIBRARY )
+    message( Found TetGenLib_LIBRARY: ${TetGenLib_LIBRARY})
+    set(TetGenLib_LIBRARY_DIR "")
+    get_filename_component(TetGenLib_LIBRARY_DIR ${TetGenLib_LIBRARY} PATH)
+    # Set uncached variables as per standard.
+    set(TetGenLib_FOUND ON)
+    set(TetGenLib_INCLUDE_DIR ${TetGenLib_INCLUDE_DIR})
+    set(TetGenLib_LIBRARY ${TetGenLib_LIBRARY})
+  endif(TetGenLib_LIBRARY)
+endif(TetGenLib_INCLUDE_DIR)
+message( FOUnd TETGEN ${TetGenLib_FOUND})
+	    
+if(TetGenLib_FOUND)
+  if(NOT TetGenLib_FIND_QUIETLY)
+    message(STATUS "FindTetGenLib: done!")
+  endif(NOT TetGenLib_FIND_QUIETLY)
+else(TetGenLib_FOUND)
+  if(TetGenLib_FIND_REQUIRED)
+    message(FATAL_ERROR "FindTetGenLib: Could not find TetGenLib libraries")
+  endif(TetGenLib_FIND_REQUIRED)
+endif(TetGenLib_FOUND)
