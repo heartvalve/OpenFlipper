6f2c094d816d9666c4dd09acae83f7d758cfd071
tenter <tenter@383ad7c9-94d9-4d36-a494-682f7c89f535>
2014-04-07 15:10:12 +0000

    - safety check in msaa sample weight computation - add msaa shaders

---
+++ OpenFlipper/Shaders/MSAA/sample_nearest.glsl
@@ -0,0 +1,16 @@
+#version 150
+
+#include "msaa.glsl"
+
+
+uniform sampler2DMS inputTex;
+uniform int numSamples;
+
+in vec2 vTexCoord;
+out vec4 outFragment;
+
+
+void main()
+{
+  outFragment = ReadMultisampledTexture(inputTex, vTexCoord, numSamples);
+}
