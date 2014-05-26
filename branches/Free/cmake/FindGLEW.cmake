1dfc6dacbe85645e34381bc816124e4b60581d3b
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2014-05-26 11:36:41 +0000

    Automatic finding of glew and glut on windows in our build tree Taucs: Not fatal if not found!

--- branches/Free/cmake/FindGLEW.cmake
+++ branches/Free/cmake/FindGLEW.cmake
@@ -13,18 +13,40 @@ IF (GLEW_INCLUDE_DIR)
 ENDIF (GLEW_INCLUDE_DIR)
 
 if( WIN32 )
+
+  if ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*Win64" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2012/x64/")
+  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2012/x32/")
+  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*Win64" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2013/x64/")
+  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2013/x32/")
+  endif()
+
    if( MSVC80 )
        set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ 8/VC" )
    endif( MSVC80 )
    if( MSVC71 )
        set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ .NET\ 2003/Vc7" )
    endif( MSVC71 )
+
    FIND_PATH( GLEW_INCLUDE_DIR gl/glew.h gl/wglew.h
-              PATHS "c:/glew/include" "c:/libs/glew/include" "c:/libs/glew-1.6.0/include" ${COMPILER_PATH}/PlatformSDK/Include )
+              PATHS "c:/glew/include" 
+                    "c:/libs/glew/include" 
+                    "c:/libs/glew-1.6.0/include" 
+                    ${COMPILER_PATH}/PlatformSDK/Include 
+                    "${VS_SEARCH_PATH}glew-1.10.0/include")
+
    SET( GLEW_NAMES glew32 )
    FIND_LIBRARY( GLEW_LIBRARY
                  NAMES ${GLEW_NAMES}
-                 PATHS "c:/glew/lib"  "c:/libs/glew/lib" "c:/libs/glew-1.6.0/lib" ${COMPILER_PATH}/PlatformSDK/Lib )
+                 PATHS "c:/glew/lib"  i
+                       "c:/libs/glew/lib" 
+                       "c:/libs/glew-1.6.0/lib" 
+                       ${COMPILER_PATH}/PlatformSDK/Lib 
+                       "${VS_SEARCH_PATH}glew-1.10.0/lib" )
+
 else( WIN32 )
    FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h GL/wglew.h
               PATHS /usr/local/include /usr/include )
