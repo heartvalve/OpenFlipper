92f5be9ed28fa73d47785e7b4202e599f6c9f6e7
moebius <moebius@383ad7c9-94d9-4d36-a494-682f7c89f535>
2014-06-25 07:35:44 +0000

    Try a smart pointer fix for Maverick with xcode 5.1.1

--- branches/Free/ACG/Utils/SmartPointer.hh
+++ branches/Free/ACG/Utils/SmartPointer.hh
@@ -80,10 +80,16 @@
      #pragma warning "TR1 not available! Please install Visual Studio Service Pack 1!"
     #endif
    #else
-     // hope for TR1 equivalents
-     #include <tr1/memory>
-     namespace ptr = std::tr1;
-     #define ACG_UNIQUE_POINTER_SUPPORTED 0
+    // hope for TR1 equivalents
+    #if defined(__APPLE__) && (MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9)
+      // Mavericks special treatment
+      #include <memory>
+      namespace ptr = std;
+    #else
+      #include <tr1/memory>
+      namespace ptr = std::tr1;
+    #endif
+    #define ACG_UNIQUE_POINTER_SUPPORTED 0
    #endif
 #endif
 
