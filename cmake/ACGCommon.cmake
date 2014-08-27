93a2f810440b6e540d3be4d502d05fd6743b2af0
moeller <moeller@383ad7c9-94d9-4d36-a494-682f7c89f535>
2014-08-27 12:40:19 +0000

    fix cmake (>=3.0) warning on MacOS
    closes #2251

--- cmake/ACGCommon.cmake
+++ cmake/ACGCommon.cmake
@@ -127,12 +127,22 @@ macro (acg_set_target_props target)
       SKIP_BUILD_RPATH 0
     )
   elseif (APPLE AND NOT ACG_PROJECT_MACOS_BUNDLE)
-    set_target_properties (
-      ${target} PROPERTIES
-      INSTALL_NAME_DIR "@executable_path/../${ACG_PROJECT_LIBDIR}"
-      BUILD_WITH_INSTALL_RPATH 1
-      SKIP_BUILD_RPATH 0
-    )
+  if (CMAKE_MAJOR_VERSION  >= 3)
+      set_target_properties (
+        ${target} PROPERTIES
+        INSTALL_RPATH "@executable_path/../${ACG_PROJECT_LIBDIR}"
+        MACOSX_RPATH 1
+        BUILD_WITH_INSTALL_RPATH 1
+        SKIP_BUILD_RPATH 0
+      )  
+    else()
+      set_target_properties (
+        ${target} PROPERTIES
+        INSTALL_NAME_DIR "@executable_path/../${ACG_PROJECT_LIBDIR}"
+        BUILD_WITH_INSTALL_RPATH 1
+        SKIP_BUILD_RPATH 0
+      ) 
+    endif(CMAKE_MAJOR_VERSION  >= 3)
   elseif (NOT APPLE)
 
     set_target_properties (
