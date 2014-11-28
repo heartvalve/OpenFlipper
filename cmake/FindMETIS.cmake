4cdb663890629c17cb3a4408983ca0e329dc7bdb
moebius <moebius@informatik.rwth-aachen.de>
2014-11-28 10:32:04 +0000

    Suitesparse and Metis finder update for vs2013 versions

--- cmake/FindMETIS.cmake
+++ cmake/FindMETIS.cmake
@@ -10,6 +10,38 @@ if (METIS_INCLUDE_DIR)
   SET(METIS_FIND_QUIETLY TRUE)
 endif (METIS_INCLUDE_DIR)
 
+if ( WIN32 ) 
+
+ if ( CMAKE_GENERATOR MATCHES ".*Win64" )
+   SET( DIRSUFFIX "lib64" )
+ else ()
+   SET( DIRSUFFIX "lib" )
+ endif()
+
+  if ( CMAKE_GENERATOR MATCHES "^Visual Studio 10.*" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2010/x32/")
+  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*Win64" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2012/x64/")
+  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 11.*" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2012/x32/")
+  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*Win64" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2013/x64/")
+  elseif ( CMAKE_GENERATOR MATCHES "^Visual Studio 12.*" )
+    SET(VS_SEARCH_PATH "c:/libs/vs2013/x32/")
+  endif()
+
+find_path(METIS_INCLUDE_DIR NAMES metis.h
+     PATHS "$ENV{IPOPT_HOME}/ThirdParty/Metis/metis-4.0/Lib/"        
+	       "${VS_SEARCH_PATH}suitesparse-metis-for-windows-1.2.2-install/include/"
+           
+   )
+   
+find_library( METIS_LIBRARY 
+              metis metis
+              PATHS "${VS_SEARCH_PATH}suitesparse-metis-for-windows-1.2.2-install/${DIRSUFFIX}" )
+					
+else(WIN32)
+
 find_path(METIS_INCLUDE_DIR NAMES metis.h
      PATHS "$ENV{IPOPT_HOME}/ThirdParty/Metis/metis-4.0/Lib/"
            "/usr/include/"
@@ -25,6 +57,8 @@ find_library( METIS_LIBRARY
               PATHS "$ENV{IPOPT_HOME}/lib/"
                     "/usr/lib"
                     "/opt/local/lib" )
+					
+endif()					
 
 set(METIS_INCLUDE_DIRS "${METIS_INCLUDE_DIR}" )
 set(METIS_LIBRARIES "${METIS_LIBRARY}" )
