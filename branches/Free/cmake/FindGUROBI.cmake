9adb37e5061758bbeb149173ea5ce8ee4f51a204
moeller <moeller@383ad7c9-94d9-4d36-a494-682f7c89f535>
2014-12-05 13:36:30 +0000

    fix variable name

--- branches/Free/cmake/FindGUROBI.cmake
+++ branches/Free/cmake/FindGUROBI.cmake
@@ -67,7 +67,7 @@ find_library( GUROBI_CXX_LIBRARY
 # Binary dir for DLLs			
 find_path(GUROBI_BIN_DIR 
                 NAMES "gurobi60.dll" 
-                PATHS "${GUROBI_INCLUDE_DIR }/../bin"
+                PATHS "${GUROBI_INCLUDE_DIR}/../bin"
 				      "${GUROBI_BASE}/bin"
                 DOC "Directory containing the GUROBI DLLs"
                ) 		  
