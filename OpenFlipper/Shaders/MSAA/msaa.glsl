dfdd8b6772b460d40b6ec7d2c60165ec18736b48
tenter <tenter@383ad7c9-94d9-4d36-a494-682f7c89f535>
2015-03-24 14:17:34 +0000

    fix shaders for resolving multisampled textures

--- OpenFlipper/Shaders/MSAA/msaa.glsl
+++ OpenFlipper/Shaders/MSAA/msaa.glsl
@@ -14,9 +14,9 @@ vec4 ReadMultisampledTexture(in sampler2DMS tex, in ivec2 pos, in int numSamples
   return texel / float(numSamples);
 }
 
-vec4 ReadMultisampledTexture(in sampler2DMS tex, in vec2 texcood, in int numSamples)
+vec4 ReadMultisampledTexture(in sampler2DMS tex, in vec2 texcoord, in int numSamples)
 {
-  ivec2 pos = int(floor(texcoord));
+  ivec2 pos = ivec2(texcoord * vec2(textureSize(tex)));
   return ReadMultisampledTexture(tex, pos, numSamples);
 }
 
@@ -35,9 +35,9 @@ vec4 ReadMultisampledTexture(in sampler2DMS tex, in ivec2 pos, in int numSamples
   return texel;
 }
 
-vec4 ReadMultisampledTexture(in sampler2DMS tex, in vec2 texcood, in int numSamples, in samplerBuffer filterWeights)
+vec4 ReadMultisampledTexture(in sampler2DMS tex, in vec2 texcoord, in int numSamples, in samplerBuffer filterWeights)
 {
-  ivec2 pos = int(floor(texcoord));
+  ivec2 pos = ivec2(texcoord * vec2(textureSize(tex)));
   return ReadMultisampledTexture(tex, pos, numSamples, filterWeights);
 }
 
